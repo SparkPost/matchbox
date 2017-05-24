ed6b6afdfa9311af36be10546e453f76ca6c1252
Jon Ambas <jon@jonambas.com>
2017-05-24 14:56:39 -0400

    Fix storybook backgrounds addon

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,4 @@
 import { configure } from '@kadira/storybook';
-import '@kadira/storybook/addons';
-import 'react-storybook-addon-backgrounds/register';
 
 function loadStories() {
   require('../stories');
