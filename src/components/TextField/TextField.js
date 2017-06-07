92231f009c4755e8d381f84e3feb9ae08e82976f
Jon Ambas <jon@jonambas.com>
2017-06-07 10:34:52 -0400

    Update TextField comments & Add textarea story

--- src/components/TextField/TextField.js
+++ src/components/TextField/TextField.js
@@ -4,22 +4,29 @@ import Error from './Error';
 import classnames from 'classnames';
 import styles from './TextField.module.scss';
 
+// TODO
+// prefix & suffix
+// error icon
+// do we need to set refs
+// do we need to accept height along with multiline
+// a11y attributes
+
 class TextField extends Component {
   render() {
     const {
       id,
-      multiline,  // textarea || input
-      type,
-      placeholder,
-      labelHidden,
-      autoFocus,
-      disabled,
-      readOnly,
+      multiline,    // textarea || input
+      type,         // string
+      placeholder,  // string
+      autoFocus,    // boolean
+      disabled,     // boolean
+      readOnly,     // boolean
 
-      error,
       prefix,
       suffix,
-      label,
+      label,        // string
+      labelHidden,  // boolean
+      error,        // string
 
       // Events
       onChange,
