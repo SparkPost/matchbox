7cb258af36c5a5671d0d7ee2154b8397a0382d29
Jon Ambas <jon@jonambas.com>
2017-06-26 22:47:37 -0400

    Add pagination button builder

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -2,61 +2,115 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
-import { Button, buttonFrom } from '../Button';
+import { Button, buttonsFrom } from '../Button';
 import { Icon } from '../Icon';
 import styles from './Pagination.module.scss';
 
-const Pagination = ({
-  hasPrevious,
-  hasNext,
-  onPrevious,
-  onNext,
-  pages
-}) => {
+class Pagination extends Component {
+  static propTypes = {
+    pages: PropTypes.number.isRequired,
+    pageRange: PropTypes.number.isRequired,
+    onChange: PropTypes.func,
+    initialIndex: PropTypes.number.isRequired
+  };
 
-  const createPages = () => (
-    pages.map(({ page, selected, onSelect }) => (
-      buttonFrom({
-        content: page,
-        className: selected && styles.Selected,
-        onClick: onSelect
-      })
-    ))
-  );
+  static defaultProps = {
+    initialIndex: 0
+  };
 
-  const pageMarkup = pages
-    ? <span className={styles.Pages}>{ createPages() }</span>
-    : null;
+  constructor(props) {
+    super(props);
+    this.state = {
+      index: 0,
+      hasPrevious: false,
+      hasNext: true
+    };
+  }
 
-  return (
-    <div className={styles.Pagination}>
-      <Button
-        className={styles.Button}
-        onClick={onPrevious}
-        disabled={!hasPrevious} >
-        <Icon name='ArrowBack' size={16} />
-      </Button>
-      { pageMarkup }
-      <Button
-        className={styles.Button}
-        onClick={onNext}
-        disabled={!hasNext} >
-        <Icon name='ArrowForward' size={16} />
-      </Button>
-    </div>
-  );
-};
+  componentWillMount() {
+    this.handlePageChange(this.props.initialIndex);
+  }
 
-Pagination.propTypes = {
-  hasPrevious: PropTypes.bool,
-  hasNext: PropTypes.bool,
-  onPrevious: PropTypes.func,
-  onNext: PropTypes.func,
-  pages: PropTypes.arrayOf(PropTypes.shape({
-    page: PropTypes.number.isRequired,
-    onSelect: PropTypes.func.isRequired,
-    selected: PropTypes.bool
-  })),
+  handlePageChange(index) {
+    this.setState({
+      index,
+      hasPrevious: index !== 0,
+      hasNext: index !== this.props.pages - 1
+    });
+
+    this.props.onChange && this.props.onChange(index);
+  }
+
+  handleNext() {
+    this.handlePageChange(this.state.index + 1);
+  }
+
+  handlePrevious() {
+    this.handlePageChange(this.state.index - 1);
+  }
+
+  _createButtons(start, end) {
+    let buttons = [];
+
+    for (let index = start; index < end; index++) {
+      buttons.push({
+        content: `${index + 1}`,
+        onClick: this.handlePageChange.bind(this, index),
+        className: index === this.state.index && styles.Selected
+      });
+    }
+
+    return buttonsFrom(buttons);
+  }
+
+  render() {
+    const {
+      pages,
+      pageRange,
+      onChange,
+      initialIndex
+    } = this.props;
+
+    const {
+      hasPrevious,
+      hasNext
+    } = this.state;
+
+    const buttonMarkup = () => {
+      if (pages <= pageRange) {
+        return this._createButtons(0, pages);
+      } else {
+        const half = Math.floor(pageRange / 2);
+        let start = 0
+
+        if (this.state.index > half) {
+          start = this.state.index - half;
+        }
+         if (this.state.index + half + 1 > pages) {
+          start = pages - pageRange;
+        }
+        return this._createButtons(start, start + pageRange);
+      }
+    };
+
+    return (
+      <div className={styles.Pagination}>
+        <Button
+          className={styles.Button}
+          onClick={() => this.handlePrevious()}
+          disabled={!hasPrevious} >
+          <Icon name='ArrowBack' size={16} />
+        </Button>
+        <span className={styles.Pages}>{ buttonMarkup() }</span>
+        <Button
+          className={styles.Button}
+          onClick={() => this.handleNext()}
+          disabled={!hasNext} >
+          <Icon name='ArrowForward' size={16} />
+        </Button>
+      </div>
+    );
+  }
 };
 
 export default Pagination;
--- stories/Pagination.js
+++ stories/Pagination.js
@@ -4,36 +4,28 @@ import { StoryContainer } from './helpers';
 
 import { Pagination } from '../src';
 
-const pages = [
-  {
-    page: 1,
-    selected: false,
-    onSelect: action('Select')
-  },
-  {
-    page: 2,
-    selected: true,
-    onSelect: action('Select')
-  },
-  {
-    page: 3,
-    selected: false,
-    onSelect: action('Select')
-  }
-];
-
 export default storiesOf('Pagination', module)
   .add('with a title', () => (
     <StoryContainer>
+
       <Pagination
-        hasPrevious={true}
-        hasNext={true}
+        pages={4}
+        pageRange={5}
+        initialIndex={2}
+        onChange={(selected) => {
+          console.log(selected, ' in story')
+          action('sdf')
+        }}
       />
 
       <Pagination
-        pages={pages}
-        hasPrevious={true}
-        hasNext={true}
+        pages={11}
+        pageRange={5}
+        initialIndex={3}
+        onChange={(selected) => {
+          console.log(selected, ' in story')
+          action('sdf')
+        }}
       />
     </StoryContainer>
   ));
