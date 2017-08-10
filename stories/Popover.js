761bf07c345ddfc8fc605f2c86758be27d83a0b4
Jon Ambas <jon@jonambas.com>
2017-08-10 15:33:07 -0400

    FAD-5353 Clean up Popover story & Datepicker

--- stories/Popover.js
+++ stories/Popover.js
@@ -3,32 +3,18 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { StoryContainer } from './helpers';
 
-import { Popover, Button, TextField } from '../src';
+import { Popover, Datepicker, Button, TextField } from '../src';
 
 export default storiesOf('Popover', module)
   .addDecorator((getStory) => (
     <StoryContainer>{ getStory() }</StoryContainer>
   ))
-  .addWithInfo('Default', () => {
-    const trigger = () => <Button>Trigger</Button>;
-
-    return (
+  .addWithInfo('With Datepicker', () => (
       <div>
         <Popover
-          trigger={<Button onClick={action('Trigger Click')}>Trigger</Button>}
-          sectioned
+          trigger={<TextField onClick={action('Trigger Click')} value={new Date}/>}
           style={{ width: '400px' }}>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit unde eius eligendi, quod sed maxime quis nam error suscipit.</p>
-          <Button primary>Yup</Button>
-        </Popover>
-        <br/><br/><br/><br/><br/><br/>
-        <Popover
-          trigger={<TextField onClick={action('Trigger Click')} />}
-          sectioned
-          style={{ width: '400px' }}>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit unde eius eligendi, quod sed maxime quis nam error suscipit.</p>
-          <Button primary>Yup</Button>
+          <Datepicker />
         </Popover>
       </div>
-    );
-  });
+    ));
