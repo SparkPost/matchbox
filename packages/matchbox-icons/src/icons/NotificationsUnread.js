feb5772e31baf734a38f2ae9ac7e82fbcdbd997f
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2018-05-08 22:03:15 -0400

    Create NotificationsUnread.js

---
+++ packages/matchbox-icons/src/icons/NotificationsUnread.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { createSvgIcon } from '../IconBase';
+
+export default createSvgIcon(
+  <g transform="translate(4.000000, 1.000000)">
+    <path d="M8,21 C9.1,21 10,20.1 10,19 L6,19 C6,20.1 6.89,21 8,21 Z M6.00263347,3.82050695 C3.44243033,4.66541678 2,7.10262748 2,10 L2,15 L0,17 L0,18 L16,18 L16,17 L14,15 L14,10 C14,9.88627792 13.9977496,9.77324193 13.9932702,9.66096269 C13.3696151,9.88055264 12.6987568,10 12,10 C8.6862915,10 6,7.3137085 6,4 C6,3.93995892 6.0008819,3.88012382 6.00263347,3.82050695 Z" id="Bell"></path>
+    <circle id="Signal" fill="currentColor" cx="12" cy="4" r="4"></circle>
+  </g>,
+  'NotificationsUnread'
+);
