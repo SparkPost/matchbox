3b524414442ee1d1310625414aa719fa402280a0
mhossain <mohammad.hossain@sparkpost.com>
2018-06-14 14:28:39 -0400

    Add tests for Pagination component

---
+++ packages/matchbox/src/components/Pagination/tests/Pagination.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Pagination from '../Pagination';
+
+describe('Pagination', () => {
+  let props;
+  let wrapper;
+
+  beforeEach(() => {
+    props = {
+      currentPage: 1,
+      pages: 10,
+      pageRange: 3,
+      selectedColor: 'blue'
+    };
+
+    wrapper = shallow(<Pagination {...props}/>);
+  });
+
+  it('renders pagination', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('invokes onChange on page', () => {
+    const fn = jest.fn();
+    wrapper.setProps({ onChange: fn });
+    wrapper.find('Button').at(0).simulate('click');
+    expect(fn).toHaveBeenCalledTimes(1);
+  });
+
+  it('renders pagination with marginsHidden true', () => {
+    wrapper.setProps({ marginsHidden: true });
+    expect(wrapper).toMatchSnapshot();
+  });
+});
