2064788839e0580210362a3f3ebbc85fda87f0e5
mhossain <mohammad.hossain@sparkpost.com>
2018-06-14 19:39:53 -0400

    Add tests for Select component

---
+++ packages/matchbox/src/components/Select/tests/Select.test.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import cases from 'jest-in-case';
+
+import Select from '../Select';
+
+describe('Select', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    const props = {
+      options: ['option 1']
+    };
+
+    wrapper = shallow(<Select {...props} />);
+  });
+
+  const testCases = [
+    { name: 'disabled', props: { disabled: true }},
+    { name: 'required', props: { required: true, label: 'Select Label' }},
+    { name: 'label', props: { label: 'Select Label' }},
+    { name: 'helpText', props: { label: 'Select help text' }},
+    { name: 'error', props: { label: 'An error occurred' }},
+    { name: 'errorInLabel', props: { error: 'Error occurred', errorInLabel: true }},
+    { name: 'with options', props: { options: ['option 1', 'option 2']}}
+  ];
+
+  cases('renders Select', (opts) => {
+    wrapper.setProps(opts.props);
+    expect(wrapper).toMatchSnapshot();
+  }, testCases);
+
+  cases('invokes event', (opts) => {
+    const fn = jest.fn();
+    const newProps = {};
+    newProps[opts.name] = fn;
+    wrapper.setProps(newProps);
+    wrapper.find('select').simulate(opts.event, opts.args); //due to enzyme no propagating event, it needs to trigger on exact element
+    expect(fn).toHaveBeenCalledTimes(1);
+  }, [
+    { name: 'onChange', event: 'change' },
+    { name: 'onBlur', event: 'blur' },
+    { name: 'onFocus', event: 'focus' }
+  ]);
+
+});
