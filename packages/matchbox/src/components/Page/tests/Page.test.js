3c636b28a84c57a69d2e0f620b085812ce132811
mhossain <mohammad.hossain@sparkpost.com>
2018-06-14 14:20:34 -0400

    Add tests for Page component

---
+++ packages/matchbox/src/components/Page/tests/Page.test.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Page from '../Page';
+
+describe('Page', () => {
+  let props;
+  let wrapper;
+
+  beforeEach(() => {
+    props = {
+      title: 'Page Title',
+      primaryAction: { content: 'Primary' }
+    };
+
+    wrapper = shallow(<Page {...props}><h1>Test Example</h1></Page>);
+  });
+
+  it('renders page with children', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('renders page with secondary actions', () => {
+    wrapper.setProps({ secondaryActions: [{ content: 'secondary 1' }, { content: 'secondary 2' }]});
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('renders page with breadcrumb', () => {
+    wrapper.setProps({ breadcrumbAction: { content: 'Step 1' }});
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('renders page with empty state', () => {
+    wrapper.setProps({ empty: { show: true, content: 'Empty page' }});
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('renders w/o title', () => {
+    wrapper.setProps({ title: null });
+    expect(wrapper).toMatchSnapshot();
+  });
+
+});
