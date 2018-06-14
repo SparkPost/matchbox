1431161c804ab588d6627fdda3189be78d45f5f6
mhossain <mohammad.hossain@sparkpost.com>
2018-06-13 21:30:06 -0400

    Add tests for CodeBlock component

---
+++ packages/matchbox/src/components/CodeBlock/tests/CodeBlock.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import CodeBlock from '../CodeBlock';
+import { shallow } from 'enzyme';
+
+describe('CodeBlock', () => {
+  let wrapper;
+  let props;
+  beforeEach(() => {
+    props = {
+      code: 'curl -X POST\nhttps://api.sparkpost.com/api/v1/tran…\n'
+    };
+
+    wrapper = shallow(<CodeBlock {...props}/>);
+  });
+
+  it('renders with default props', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('renders line numbers', () => {
+    wrapper.setProps({ numbered: true, height: 200 });
+    expect(wrapper).toMatchSnapshot();
+  });
+
+});
