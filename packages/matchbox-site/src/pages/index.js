0c5255657ffcfa5f09a4108d8664f9a931632fdd
Jon Ambas <jon@jonambas.com>
2018-06-29 17:20:20 -0400

    Tweak styles

--- packages/matchbox-site/src/pages/index.js
+++ packages/matchbox-site/src/pages/index.js
@@ -1,5 +1,4 @@
-import React, { Fragment } from 'react';
-import Link from 'gatsby-link';
+import React from 'react';
 import { Button, UnstyledLink } from '@sparkpost/matchbox';
 import GithubIcon from '../components/GithubIcon';
 
@@ -9,12 +8,12 @@ const IndexPage = () => (
   <div>
     <h1 className={styles.Title}>Matchbox</h1>
     <p className={styles.Description}>
-      A React component library built by <UnstyledLink to='https://www.sparkpost.com'>SparkPost</UnstyledLink>.
+      A React component library built by <UnstyledLink className={styles.Link} to='https://www.sparkpost.com'>SparkPost</UnstyledLink>.
     </p>
 
     <div className={styles.ButtonWrapper}>
       <Button size='large' color='orange' to='https://sparkpost.github.io/matchbox/'>Components</Button>
-      <Button size='large' to='https://github.com/SparkPost/matchbox'><GithubIcon /> Github</Button>
+      <Button size='large' outline to='https://github.com/SparkPost/matchbox'><GithubIcon /> Github</Button>
     </div>
   </div>
 );
