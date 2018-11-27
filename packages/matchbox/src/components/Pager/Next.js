0697a369ebf94f11e9e59e8fcedceb816217cf61
Brian Kemper <brian.kemper@sparkpost.com>
2018-11-27 15:36:21 -0500

    EO-423 Pass-through Pager Next and Previous button props to Button

--- packages/matchbox/src/components/Pager/Next.js
+++ packages/matchbox/src/components/Pager/Next.js
@@ -1,19 +1,13 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { ArrowForward } from '@sparkpost/matchbox-icons';
 import { Button } from '../Button';
 import styles from './Pager.module.scss';
 
-const Next = ({ disabled, onClick }) => (
-  <Button className={styles.Next} disabled={disabled} onClick={onClick}>
+const Next = (props) => (
+  <Button {...props} className={styles.Next}>
     <ArrowForward size={16} />
   </Button>
 );
-
-Next.propTypes = {
-  disabled: PropTypes.bool,
-  onClick: PropTypes.func
-};
 
 Next.displayName = 'Pager.Next';
 
--- packages/matchbox/src/components/Pager/Previous.js
+++ packages/matchbox/src/components/Pager/Previous.js
@@ -1,19 +1,13 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { ArrowBack } from '@sparkpost/matchbox-icons';
 import { Button } from '../Button';
 import styles from './Pager.module.scss';
 
-const Previous = ({ disabled, onClick }) => (
-  <Button className={styles.Previous} disabled={disabled} onClick={onClick}>
+const Previous = (props) => (
+  <Button {...props} className={styles.Previous}>
     <ArrowBack size={16} />
   </Button>
 );
-
-Previous.propTypes = {
-  disabled: PropTypes.bool,
-  onClick: PropTypes.func
-};
 
 Previous.displayName = 'Pager.Previous';
 
