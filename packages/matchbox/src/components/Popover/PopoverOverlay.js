f651dd07b443f8eb464409763aeb75c89d2d0d50
Jon Ambas <jon@jonambas.com>
2019-03-08 10:38:56 -0500

    FE-574 Bump debounce

--- packages/matchbox/src/components/Popover/PopoverOverlay.js
+++ packages/matchbox/src/components/Popover/PopoverOverlay.js
@@ -50,7 +50,7 @@ class PopoverOverlay extends Component {
 
     return (
       <Fragment>
-        <WindowEvent event='resize' handler={debounce(this.handleMeasurement, 1000)} />
+        <WindowEvent event='resize' handler={debounce(this.handleMeasurement, 2000)} />
         {(!fixed && open) ? <WindowEvent event='scroll' handler={this.handleMeasurement} /> : null}
         {renderActivator(activatorProps)}
         <Portal containerId={portalId}>
