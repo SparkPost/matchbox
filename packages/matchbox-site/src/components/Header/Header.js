f0a166358c8b903efb0a4a6111e09837f79bd1f4
Jon Ambas <jon@jonambas.com>
2019-07-31 11:21:13 -0400

    Add routes, layout, placeholder pages

---
+++ packages/matchbox-site/src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import MainNavigation from "../MainNavigation/MainNavigation";
+import { Link } from "gatsby";
+import styles from './Header.module.scss';
+
+function Header(props) {
+  return (
+    <header className={styles.Header}>
+      <Link to='/' className={styles.Title}><h1>Matchbox</h1></Link>
+      <div className={styles.NavWrapper}>
+        <MainNavigation
+          navItems={props.navItems}
+          location={props.location}
+        />
+      </div>
+    </header>
+  )
+}
+
+export default Header;
