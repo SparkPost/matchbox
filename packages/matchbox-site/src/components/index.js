395664853cc80fa1cae68170b085349647537b6e
Jon Ambas <jon@jonambas.com>
2019-08-07 15:07:47 -0400

    UX-139 feat(site): Add Token usage component

--- packages/matchbox-site/src/components/index.js
+++ packages/matchbox-site/src/components/index.js
@@ -4,6 +4,7 @@ import MainNavigation from './MainNavigation/MainNavigation';
 import MDXProvider from './MDXProvider';
 import SideNavigation from './SideNavigation/SideNavigation';
 import Token from './tokens/Token';
+import TokenUsage from './tokens/TokenUsage';
 import TypographyExplorer from './TypographyExplorer/TypographyExplorer';
 
-export { Header, Layout, MainNavigation, MDXProvider, SideNavigation, Token, TypographyExplorer };
+export { Header, Layout, MainNavigation, MDXProvider, SideNavigation, Token, TokenUsage, TypographyExplorer };
