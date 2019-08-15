10440b861b3bacf8188422f476003dae5a268f60
Nick Lemmon <nick.lemmon@sparkpost.com>
2019-08-15 09:12:10 -0400

    Add additional supporting test to check that the element is inline vs block

--- packages/matchbox/src/components/ScreenReaderOnly/tests/ScreenReaderOnly.test.js
+++ packages/matchbox/src/components/ScreenReaderOnly/tests/ScreenReaderOnly.test.js
@@ -8,4 +8,10 @@ describe('ScreenReaderOnly', () => {
 
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('Renders the component using as a `<span></span>` to ensure it can be used in all situations (i.e., within other inline elements)', () => {
+    const wrapper = shallow(<ScreenReaderOnly>I will surely render as a span!</ScreenReaderOnly>);
+
+    expect(wrapper.find('span')).toHaveLength(1);
+  });
 });
