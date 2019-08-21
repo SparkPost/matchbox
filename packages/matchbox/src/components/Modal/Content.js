fd6da01eaa2d03affa4762e39dfd621e8c88da33
Nick Lemmon <nick.lemmon@sparkpost.com>
2019-08-21 14:53:10 -0400

    Implement focus lock in modal component

--- packages/matchbox/src/components/Modal/Content.js
+++ packages/matchbox/src/components/Modal/Content.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Transition } from 'react-transition-group';
+import FocusLock from 'react-focus-lock';
 import classnames from 'classnames';
 import styles from './Modal.module.scss';
 
 const Content = ({ children, contentRef, open }) => (
-  <Transition mountOnEnter unmountOnExit in={open} timeout={{ enter: 0, exit: 150 }}>
-    {(state) => {
-      const classes = classnames(styles.Content, state && styles[state]);
-      return <div className={classes} ref={contentRef}>{children}</div>;
-    }}
-  </Transition>
+  <FocusLock>
+    <Transition mountOnEnter unmountOnExit in={open} timeout={{ enter: 0, exit: 150 }}>
+      {(state) => {
+        const classes = classnames(styles.Content, state && styles[state]);
+        return <div className={classes} ref={contentRef}>{children}</div>;
+      }}
+    </Transition>
+  </FocusLock>
 );
 
 Content.displayName = 'Modal.Content';
