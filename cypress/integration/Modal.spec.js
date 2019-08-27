b2f46b00bfe9e9bd262369c8dd904cb94de33f64
Nick Lemmon <nick.lemmon@sparkpost.com>
2019-08-27 16:22:02 -0400

    Issue 214 Add Cypress tests for modal update, incorporate Cypress in to CI

--- cypress/integration/Modal.spec.js
+++ cypress/integration/Modal.spec.js
@@ -6,13 +6,13 @@ describe('The Modal component', () => {
     cy.visit('/iframe.html?selectedKind=Overlays%7CModal&selectedStory=Toggle%20Example&full=0&addons=1&stories=1&panelRight=0&addonPanel=storybook%2Factions%2Factions-panel');
   });
 
-  it('Opens when clicking the "Open modal" button', () => {
+  it('opens when clicking the "Open modal" button', () => {
     cy.get('[data-test="open-modal"]').click();
 
     cy.contains('Are you sure you want to delete your template?').should('be.visible');
   });
 
-  it('Traps focus within the modal when rendered', () => {
+  it('traps focus within the modal when rendered', () => {
     cy.get('[data-test="open-modal"]').click();
 
     // Wait for transition animation
