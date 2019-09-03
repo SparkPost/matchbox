750225cb366c2be69f6fc426d44a4648f7b5db26
Jon Ambas <jon@jonambas.com>
2019-09-03 15:44:48 -0400

    Add PropsTable, react doc gen, and test component pages

--- packages/matchbox-site/gatsby-config.js
+++ packages/matchbox-site/gatsby-config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   siteMetadata: {
     title: 'Matchbox',
@@ -42,6 +44,14 @@ module.exports = {
         theme_color: '#663399',
         display: 'minimal-ui',
         icon: 'src/images/gatsby-icon.png' // This path is relative to the root of the site.
+      }
+    },
+    'gatsby-transformer-react-docgen',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'components',
+        path: path.resolve(__dirname, '../matchbox/src/components')
       }
     }
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- packages/matchbox-site/src/components/MDXProvider.js
+++ packages/matchbox-site/src/components/MDXProvider.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { MDXProvider } from '@mdx-js/react';
 import { Link } from 'gatsby';
+import { PropsTable } from './PropsTable';
 
 const components = {
-  a: (props) => <Link to={props.href}>{props.children}</Link>
+  a: (props) => <Link to={props.href}>{props.children}</Link>,
+  PropsTable
 };
 
 export default ({ children }) => (
