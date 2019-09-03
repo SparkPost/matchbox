14ad97cbc347ae12f41d8821f64720e6ff2540d0
Nick Lemmon <nick.lemmon@sparkpost.com>
2019-09-03 11:54:08 -0400

    Issue 219 Incorporate ScreenReaderOnly component and relevant snapshots

--- packages/matchbox/src/components/Pager/Previous.js
+++ packages/matchbox/src/components/Pager/Previous.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { ArrowBack } from '@sparkpost/matchbox-icons';
 import { Button } from '../Button';
+import { ScreenReaderOnly } from '../ScreenReaderOnly';
 import styles from './Pager.module.scss';
 
 const Previous = (props) => (
   <Button {...props} className={styles.Previous}>
     <ArrowBack size={16} />
+
+    <ScreenReaderOnly>Previous</ScreenReaderOnly>
   </Button>
 );
 
