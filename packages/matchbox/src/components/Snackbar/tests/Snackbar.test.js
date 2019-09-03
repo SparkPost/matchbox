681c1fd70a83014640f15b9eda31d3834664eb0c
Nick Lemmon <nick.lemmon@sparkpost.com>
2019-09-03 11:20:08 -0400

    Issue 234 Add explicit tests that support the addition of new icons

--- packages/matchbox/src/components/Snackbar/tests/Snackbar.test.js
+++ packages/matchbox/src/components/Snackbar/tests/Snackbar.test.js
@@ -26,6 +26,49 @@ describe('Snackbar', () => {
     expect(wrapper).toMatchSnapshot();
   });
 
+  it('Renders the "Info" icon with the "default" status with a `label` prop value of "Info"', () => {
+    wrapper.setProps({ status: 'default' });
+
+    const infoIcon = wrapper.find('Info');
+
+    expect(infoIcon).toExist();
+    expect(infoIcon.props().label).toEqual('Info');
+  });
+
+  it('Renders the "CheckCircle" icon with the "success" status with a `label` prop value of "Success"', () => {
+    wrapper.setProps({ status: 'success' });
+
+    const infoIcon = wrapper.find('CheckCircle');
+
+    expect(infoIcon).toExist();
+    expect(infoIcon.props().label).toEqual('Success');
+  });
+
+  it('Renders the "Warning" icon with the "warning" status with a `label` prop value of "Warning"', () => {
+    wrapper.setProps({ status: 'warning' });
+
+    const infoIcon = wrapper.find('Warning');
+
+    expect(infoIcon).toExist();
+    expect(infoIcon.props().label).toEqual('Warning');
+  });
+
+  it('Renders the "Error" icon with the "error" or "danger" status with a `label` prop value of "Error"', () => {
+    wrapper.setProps({ status: 'error' });
+
+    const errorIcon = wrapper.find('Error');
+
+    expect(errorIcon).toExist();
+    expect(errorIcon.props().label).toEqual('Error');
+
+    wrapper.setProps({ status: 'danger' });
+
+    const dangerIcons = wrapper.find('Error');
+
+    expect(dangerIcons).toExist();
+    expect(dangerIcons.props().label).toEqual('Error');
+  });
+
   it('invokes onDismiss', () => {
     wrapper.find(`.${styles.Dismiss}`).simulate('click');
     expect(props.onDismiss).toHaveBeenCalledTimes(1);
