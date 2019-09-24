f18332c4dcf417cde5779c7c03495b0966766324
Jon Ambas <jon@jonambas.com>
2019-09-24 15:38:31 -0400

    Copy updates

--- packages/matchbox-site/src/components/MDXProvider.js
+++ packages/matchbox-site/src/components/MDXProvider.js
@@ -3,11 +3,13 @@ import { MDXProvider } from '@mdx-js/react';
 import { Link } from 'gatsby';
 import { PropsTable } from './PropsTable';
 import TokenTable from './tokens/TokenTable';
+import Token from './tokens/Token';
 
 const components = {
   a: (props) => <Link to={props.href}>{props.children}</Link>,
   PropsTable,
-  TokenTable
+  TokenTable,
+  Token
 };
 
 export default ({ children }) => (
