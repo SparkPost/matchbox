4203dfdb6229755206b4537cac6d8a06b42fd5a7
Jon Ambas <jon@jonambas.com>
2019-09-24 15:38:31 -0400

    Copy updates

--- packages/matchbox-site/src/components/SpacingPreview/SpacingPreview.js
+++ packages/matchbox-site/src/components/SpacingPreview/SpacingPreview.js
@@ -3,13 +3,13 @@ import tokens from '@sparkpost/design-tokens/dist/index.meta.json';
 import _ from 'lodash';
 import styles from './SpacingPreview.module.scss';
 
-const spacingScale = _.filter(tokens, ({ name, category }) => !name.includes('base') && category === 'spacing').reverse();
+const spacingScale = _.filter(tokens, ({ name, value, category }) => !name.includes('base') && category === 'spacing' && value !== '0rem').reverse();
 
 function SpacingPreview() {
   return (
     <div className={styles.SpacingPreview}>
       {_.map(spacingScale, ((spc) => (
-        <div className={styles.Space}>
+        <div key={spc.pixel_value} className={styles.Space}>
           <div style={{ width: spc.pixel_value }}></div>
           <p>{spc.pixel_value}</p>
         </div>
