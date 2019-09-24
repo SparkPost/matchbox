0b040cb1c22362c4b438dfe66f398ba9771ce04a
Jon Ambas <jon@jonambas.com>
2019-09-24 14:11:21 -0400

    Add spacing page token table, Adjust some hard coded values to token values

--- packages/matchbox-site/src/components/tokens/TokenTable.js
+++ packages/matchbox-site/src/components/tokens/TokenTable.js
@@ -1,27 +1,45 @@
 import React from 'react';
-import tokens from '@sparkpost/design-tokens/dist/index.meta.json';
+import styles from './TokenTable.module.scss';
+import TokenUsage from './TokenUsage';
 
-function TokenTable() {
-  function renderTokenRow(token, i) {
-    return (
-      <div
-        key={`${token.name}-${i}`}
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'space-between'
-        }}
-      >
-        <strong>{token.friendly}</strong>
-        <p>{token.type}</p>
-        <p>{token.css}</p>
-        <p>{token.scss}</p>
-        <p>{token.value}</p>
-      </div>
-    );
+function TokenTable(props) {
+  const [usageType, setType] = React.useState('scss');
+
+  function handleTypeSelect(e) {
+    setType(e.target.value);
   }
 
-  return <div>{tokens.map(renderTokenRow)}</div>;
+  function renderTokenRow(token, i) {
+
+    const cells = props.columns.map((type) => {
+      let cell = token[type];
+
+      if (type === 'usage') {
+        cell = <TokenUsage usage={token[usageType] || token.value}/>;
+      }
+
+      return <td key={type} className={styles[type]}>{cell}</td>;
+    });
+
+    return <tr key={`${token.name}-${i}`}>{cells}</tr>;
+  }
+
+  return (
+    <>'     '<div className={styles.Header}>
+      <h4>Tokens</h4>
+      <div>
+        <select onChange={handleTypeSelect} value={usageType}>
+          <option value='scss'>Scss</option>
+          <option value='css'>CSS</option>
+          <option value='javascript'>Javascript</option>
+        </select>
+      </div>
+    </div>'     '<table className={styles.TokenTable}>
+      <tbody>
+        {props.tokens.map(renderTokenRow)}
+      </tbody>
+    </table>'   '</>
+  );
 }
 
 export default TokenTable;
