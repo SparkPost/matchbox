acdb9423ee77d333d5fa61ed178c3d06658cbc11
Nick Lemmon <nick.lemmon@sparkpost.com>
2019-10-22 09:50:48 -0400

    Resolve Issue #219 Incorporate ScreenReaderOnly component and relevant snapshots (#240)

--- packages/matchbox/src/components/Pager/Next.js
+++ packages/matchbox/src/components/Pager/Next.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { ArrowForward } from '@sparkpost/matchbox-icons';
 import { Button } from '../Button';
+import { ScreenReaderOnly } from '../ScreenReaderOnly';
 import styles from './Pager.module.scss';
 
 const Next = (props) => (
   <Button {...props} className={styles.Next}>
     <ArrowForward size={16} />
+
+    <ScreenReaderOnly>Next</ScreenReaderOnly>
   </Button>
 );
 
