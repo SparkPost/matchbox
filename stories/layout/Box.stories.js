9a9765df23a76665ef73ed7594f211b760ec529d
Jon Ambas <jon@jonambas.com>
2019-10-31 11:03:52 -0400

    Remove global addon-info decorator

--- stories/layout/Box.stories.js
+++ stories/layout/Box.stories.js
@@ -5,14 +5,13 @@ import { withInfo } from '@storybook/addon-info';
 import { Box } from '@sparkpost/matchbox/components/Box';
 import { ThemeProvider } from '@sparkpost/matchbox/components/ThemeProvider';
 
-addDecorator(withInfo({ propTables: [Box] }));
 addDecorator((storyFn) => <ThemeProvider>{storyFn()}</ThemeProvider>);
 
 export default {
   title: 'Layout|Box',
 };
 
-export const StyledBox = () => (
+export const StyledBox = withInfo()(() => (
   <Box
     color="gray.1000"
     fontWeight="semibold"
@@ -24,17 +23,17 @@ export const StyledBox = () => (
     border={400}>
       Just a Box
   </Box>
-);
+));
 
-export const FlexyBox = () => (
+export const FlexyBox = withInfo()(() => (
   <Box display="flex" my="400" justifyContent="space-around">
     <Box bg="magenta.400" padding="600"></Box>
     <Box bg="purple.400" padding="600"></Box>
     <Box bg="blue.400" padding="600"></Box>
   </Box>
-);
+));
 
-export const GriddyBox = () => (
+export const GriddyBox = withInfo()(() => (
   <Box display="grid" m="400" gridTemplateColumns="2fr 1fr 1fr" gridGap="400">
     <Box bg="purple.200" padding="400"></Box>
     <Box bg="purple.300" padding="400"></Box>
@@ -43,4 +42,4 @@ export const GriddyBox = () => (
     <Box bg="magenta.300" padding="400" gridColumn="1 / 3"></Box>
     <Box bg="magenta.400" padding="400" gridColumn="3 / 4"></Box>
   </Box>
-);
+));
