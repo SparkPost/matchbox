4018e91db380b05080b819215bbbfcd777dbe743
Jon Ambas <jon@jonambas.com>
2019-11-04 13:59:07 -0500

    Mock theme file globally, Remove unecessary theme provider snapshot

--- config/helpers.js
+++ config/helpers.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ThemeProvider from '../packages/matchbox/src/components/ThemeProvider/ThemeProvider';
 import { render } from 'enzyme';
+import theme from '../packages/matchbox/src/components/ThemeProvider/theme';
+
+jest.mock('../packages/matchbox/src/components/ThemeProvider/theme', () => ({
+  default: {
+    "mock-theme-key": "mock-theme-value"
+  }
+}));
 
 // jest-styled-components@6.3.3 has some issues:
 // - does not work with Enzyme shallow or mount
--- packages/matchbox/src/components/ThemeProvider/tests/ThemeProvider.test.js
+++ packages/matchbox/src/components/ThemeProvider/tests/ThemeProvider.test.js
@@ -6,10 +6,6 @@ import ThemeProvider from '../ThemeProvider';
 describe('ThemeProvider', () => {
   const subject = (props) => shallow(<ThemeProvider {...props} />);
 
-  it('should use the design system theme', () => {
-    expect(subject().prop('theme')).toMatchSnapshot();
-  });
-
   it('should be able to modify the theme', () => {
     expect(subject({ theme: { testKey: 'testValue' }}).prop('theme').testKey).toEqual('testValue');
   });
