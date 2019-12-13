4c1b4033e52dc6d2a2c28ab94ff1a9363a2dfd36
Jon Ambas <jon@jonambas.com>
2019-12-13 15:12:27 -0500

    issue-296 Remove unused action addon import

--- stories/form/Slider.stories.js
+++ stories/form/Slider.stories.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
-import { action } from '@storybook/addon-actions';
 import StoryContainer from '../storyHelpers/StoryContainer';
 
 import { Slider, TextField } from '@sparkpost/matchbox';
