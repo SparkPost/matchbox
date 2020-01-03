668474b04bc6a2fc4235f678f65887e63b8ce757
Jon Ambas <jon@jonambas.com>
2020-01-02 19:55:31 -0500

    FE-792 Add Stack component tests

--- packages/matchbox/src/components/Stack/tests/Stack.test.js
+++ packages/matchbox/src/components/Stack/tests/Stack.test.js
@@ -3,8 +3,26 @@ import Stack from '../Stack';
 import 'jest-styled-components';
 
 describe('Stack', () => {
-  it('it should render correctly', () => {
-    const wrapper = global.renderStyled(<Stack>Just a Stack</Stack>);
+  it('it should render defaults correctly', () => {
+    const wrapper = global.renderStyled(<Stack><div>1</div><div>2</div></Stack>);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('it should render with align and space props', () => {
+    const wrapper = global.renderStyled(
+      <Stack align='right' space='500'>
+        <div>1</div><div>2</div>
+      </Stack>
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('it should render with align responsive props', () => {
+    const wrapper = global.renderStyled(
+      <Stack align={['center', 'left']}>
+        <div>1</div>
+      </Stack>
+    );
     expect(wrapper).toMatchSnapshot();
   });
 });
