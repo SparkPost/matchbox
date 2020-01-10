95a800d288db4a18d2db6f5cc07836660113820e
Jon Ambas <jon@jonambas.com>
2020-01-09 22:33:19 -0500

    Add color codes to color description component

--- packages/matchbox-site/src/components/Color/ColorDescription.js
+++ packages/matchbox-site/src/components/Color/ColorDescription.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Token from '../tokens/Token';
 import { meta } from '@sparkpost/design-tokens';
 import _ from 'lodash';
-
+import color from 'color';
 import { Box, Text } from '@sparkpost/matchbox';
 
 function ColorDescription(props) {
@@ -15,13 +15,21 @@ function ColorDescription(props) {
       <Box
         mb='300'
         height='150px'
+        p='300'
         bg={c.value}
         border={isWhite ? '400' : 'none'}
         borderRadius='200'
         display='flex'
-        justifyContent='center'
-        alignItems='center'
+        justifyContent='flex-start'
+        alignItems='flex-end'
       >
+        <Box
+          fontSize='100'
+          lineHeight='100'
+          color={color(c.value).isDark() ? 'white' : 'gray.1000'}
+        >
+          {color(c.value).rgb().string()}, {c.value}
+        </Box>
       </Box>
       {props.title && (
         <Text
@@ -31,10 +39,10 @@ function ColorDescription(props) {
           fontWeight='medium'
           color='gray.900'
         >
-          {props.title}
+          {props.title} <Box display='inline-block' fontSize='200' ml='200'><Token name={c.name} /></Box>
         </Text>
       )}
-      <Token name={c.name} />
+      {!props.title && <Token name={c.name} />}
       {props.children && (
         <Box mt='100' fontSize='200' lineHeight='200' color='gray.700'>
           {props.children}
