6e9af4b02ec853a0a7408bee64edd43fbdea98d2
Neal <nstanard@gmail.com>
2020-01-10 11:02:26 -0600

    FE-776 - cypress tests for pagination components

--- cypress/integration/Pagination.spec.js
+++ cypress/integration/Pagination.spec.js
@@ -1,6 +1,20 @@
 /* eslint-disable no-undef */
 /// <reference types="Cypress" />
 
+function getElement(elements, valueToFind) {
+  let elem;
+  const keys = Object.keys(elements);
+  keys.forEach((key) => {
+    const value = parseInt(elements[key].innerText, 10);
+    // eslint-disable-next-line use-isnan
+    if (parseInt(key, 10) !== NaN && parseInt(key, 10) >= 0 && value >= 0 && value === valueToFind) {
+      elem = elements[key];
+    }
+  });
+
+  return elem;
+}
+
 describe('The Pagination component', () => {
   beforeEach(() => {
     cy.visit(
@@ -8,42 +22,77 @@ describe('The Pagination component', () => {
     );
   });
 
-  it('Changes pages!', async() => {
+  it('Changes pages!', () => {
     cy.queryByText('1').should('exist');
     cy.queryByText('2').should('exist');
     cy.queryByText('2').click();
-    cy.findAllByRole('button').then((elements) => {
-	  console.log(elements);
+    cy.queryByText('1').click();
+    cy.get('button:last-child > svg').click();
+    cy.get('button:first-child > svg').click();
+    cy.get('button:last-child > svg').click();
+    cy.get('button:last-child > svg').click();
+    cy.get('button:last-child > svg').click();
+    cy.get('button:last-child > svg').click();
+    cy.get('button:last-child > svg').click();
+    cy.get('button:last-child > svg').click();
+    cy.get('button:last-child > svg').click();
+    cy.get('button:last-child > svg').click();
+    cy.get('button:last-child > svg').click();
+    cy.get('button:last-child > svg').click();
 
-      elements.forEach((el) => {
-        el.queryByText('3');
-      });
-      //   cy.queryByText(elements, '3');
+    cy.get('button').then((elements) => {
+      cy.findByText(/^8/, { container: elements[3] }).should('exist');
     });
 
-    const buttons = await cy.findAllByRole('button');
-    console.log(buttons);
-  	// expect(buttons).to.have.lengthOf(3);
-
-    // cy.findAllByRole();
-    // const buttons = cy.findAllBy('button').then(() => {
-    //   buttons.queryByText('3').should('exist');
-    // });
-
-    // cy.get('button').within(() => {
-    //   cy.findByText('3').should('exist');
-    // });
-
+    cy.queryByText('6').click();
+    cy.queryByText('5').click();
+    cy.queryByText('4').click();
+    cy.get('button:first-child > svg').click();
+    cy.queryByText('2').click();
+    cy.queryByText('1').click();
+    cy.queryByText('2').should('exist');
+    cy.queryByText('4').should('not.exist');
   });
 });
 
-describe('The Pagination component', () => {
+describe('The Pagination component with lots of pages and flat buttons', () => {
   beforeEach(() => {
     cy.visit(
-      '?selectedKind=Navigation%7CPagination&selectedStory=with%20lots%20of%20pages%20and%20flat%20buttons&full=0&addons=1&stories=1&panelRight=0&addonPanel=storybook%2Factions%2Factions-panel'
+      '/iframe.html?selectedKind=Navigation%7CPagination&selectedStory=with%20lots%20of%20pages%20and%20flat%20buttons&full=0&addons=1&stories=1&panelRight=0&addonPanel=storybook%2Factions%2Factions-panel'
     );
   });
 
   it('Changes pages!', () => {
+    cy.queryByText('1').should('exist');
+    cy.get('button:last-child > svg').click();
+    cy.get('button:first-child > svg').click();
+    cy.get('button:last-child > svg').click();
+    cy.get('button:last-child > svg').click();
+    cy.get('button:last-child > svg').click();
+    cy.get('button:last-child > svg').click();
+    cy.get('button:last-child > svg').click();
+    cy.get('button:last-child > svg').click();
+    cy.get('button:last-child > svg').click();
+    cy.get('button:last-child > svg').click();
+    cy.get('button:last-child > svg').click();
+    cy.get('button:last-child > svg').click();
+
+    cy.get('button').then((elements) => {
+	  const elem = getElement(elements, 30);
+      cy.findByText(/^30/, { container: elem }).should('exist');
+      cy.findByText(/^30/, { container: elem }).click();
+    });
+
+    cy.get('button').then((elements) => {
+      const elem = getElement(elements, 1);
+      cy.findByText(/^1/, { container: elem }).should('exist');
+      cy.findByText(/^1/, { container: elem }).click();
+    });
+
+    cy.get('button').then((elements) => {
+      const elem = getElement(elements, 30);
+      cy.findByText(/^30/, { container: elem }).should('exist');
+      cy.findByText(/^30/, { container: elem }).click();
+    });
   });
 });
