67319a3dd0b4ad6543c503c6f282ee3c66fcc1ed
Nick Lemmon <nick.lemmon@sparkpost.com>
2020-01-14 08:49:11 -0500

    Resolves #301 - Adds Prettier support (#312)
    * incorporate-prettier - Install Prettier, update ESLint config accordingly, add VSCode settings file
    
    * incorporate-prettier - Arbitrary change
    
    * incorporate-prettier - Re-enable VSCode project settings

--- stories/action/Button.stories.js
+++ stories/action/Button.stories.js
@@ -1,91 +1,164 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
-import { action } from '@storybook/addon-actions';
 import StoryContainer from '../storyHelpers/StoryContainer';
 
 import { Button } from '@sparkpost/matchbox';
 
 export default storiesOf('Action|Button', module)
-  .addDecorator((getStory) => (
-    <StoryContainer bg='white'>{ getStory() }</StoryContainer>
-  ))
+  .addDecorator(getStory => <StoryContainer bg="white">{getStory()}</StoryContainer>)
 
-  .add('Sizing', withInfo()(() => (
-    <div>
-      <Button size='small'>Small Button</Button> &nbsp;
-      <Button>Default Button</Button> &nbsp;
-      <Button size='large'>Large Button</Button>
-    </div>
-  )))
+  .add(
+    'Sizing',
+    withInfo()(() => (
+      <div>
+        <Button size="small">Small Button</Button> &nbsp;
+        <Button>Default Button</Button> &nbsp;
+        <Button size="large">Large Button</Button>
+      </div>
+    )),
+  )
 
-  .add('Colors', withInfo()(() => (
-    <div>
-      <p>
-        <Button >Button</Button> &nbsp;
-        <Button disabled>Disabled</Button> &nbsp;
-        <Button flat>Flat</Button> &nbsp;
-        <Button flat disabled>Flat Disabled</Button>
-      </p>
-      <p>
-        <Button color='red'>Button</Button> &nbsp;
-        <Button color='red' disabled>Disabled</Button> &nbsp;
-        <Button color='red' flat>Flat</Button> &nbsp;
-        <Button color='red' flat disabled>Flat Disabled</Button>
-      </p>
-      <p>
-        <Button color='orange'>Button</Button> &nbsp;
-        <Button color='orange' disabled>Disabled</Button> &nbsp;
-        <Button color='orange' flat>Flat</Button> &nbsp;
-        <Button color='orange' flat disabled>Flat Disabled</Button>
-      </p>
-      <p>
-        <Button color='blue'>Button</Button> &nbsp;
-        <Button color='blue' disabled>Disabled</Button> &nbsp;
-        <Button color='blue' flat>Flat</Button> &nbsp;
-        <Button color='blue' flat disabled>Flat Disabled</Button>
-      </p>
-      <p>
-        <Button color='navy'>Button</Button> &nbsp;
-        <Button color='navy' disabled>Disabled</Button> &nbsp;
-        <Button color='navy' flat>Flat</Button> &nbsp;
-        <Button color='navy' flat disabled>Flat Disabled</Button>
-      </p>
-      <p>
-        <Button color='purple'>Button</Button> &nbsp;
-        <Button color='purple' disabled>Disabled</Button> &nbsp;
-        <Button color='purple' flat>Flat</Button> &nbsp;
-        <Button color='purple' flat disabled>Flat Disabled</Button>
-      </p>
-    </div>
-  )))
+  .add(
+    'Colors',
+    withInfo()(() => (
+      <div>
+        <p>
+          <Button>Button</Button> &nbsp;
+          <Button disabled>Disabled</Button> &nbsp;
+          <Button flat>Flat</Button> &nbsp;
+          <Button flat disabled>
+            Flat Disabled
+          </Button>
+        </p>
+        <p>
+          <Button color="red">Button</Button> &nbsp;
+          <Button color="red" disabled>
+            Disabled
+          </Button>{' '}
+          &nbsp;
+          <Button color="red" flat>
+            Flat
+          </Button>{' '}
+          &nbsp;
+          <Button color="red" flat disabled>
+            Flat Disabled
+          </Button>
+        </p>
+        <p>
+          <Button color="orange">Button</Button> &nbsp;
+          <Button color="orange" disabled>
+            Disabled
+          </Button>{' '}
+          &nbsp;
+          <Button color="orange" flat>
+            Flat
+          </Button>{' '}
+          &nbsp;
+          <Button color="orange" flat disabled>
+            Flat Disabled
+          </Button>
+        </p>
+        <p>
+          <Button color="blue">Button</Button> &nbsp;
+          <Button color="blue" disabled>
+            Disabled
+          </Button>{' '}
+          &nbsp;
+          <Button color="blue" flat>
+            Flat
+          </Button>{' '}
+          &nbsp;
+          <Button color="blue" flat disabled>
+            Flat Disabled
+          </Button>
+        </p>
+        <p>
+          <Button color="navy">Button</Button> &nbsp;
+          <Button color="navy" disabled>
+            Disabled
+          </Button>{' '}
+          &nbsp;
+          <Button color="navy" flat>
+            Flat
+          </Button>{' '}
+          &nbsp;
+          <Button color="navy" flat disabled>
+            Flat Disabled
+          </Button>
+        </p>
+        <p>
+          <Button color="purple">Button</Button> &nbsp;
+          <Button color="purple" disabled>
+            Disabled
+          </Button>{' '}
+          &nbsp;
+          <Button color="purple" flat>
+            Flat
+          </Button>{' '}
+          &nbsp;
+          <Button color="purple" flat disabled>
+            Flat Disabled
+          </Button>
+        </p>
+      </div>
+    )),
+  )
 
-  .add('Destructive', withInfo()(() => (
-    <div>
-      <Button destructive size='small'>Delete domain</Button> &nbsp;
-      <Button destructive>Delete domain</Button> &nbsp;
-      <Button destructive size='large'>Delete domain</Button> &nbsp;
-      <Button destructive disabled>Delete domain</Button> &nbsp;
-    </div>
-  )))
+  .add(
+    'Destructive',
+    withInfo()(() => (
+      <div>
+        <Button destructive size="small">
+          Delete domain
+        </Button>{' '}
+        &nbsp;
+        <Button destructive>Delete domain</Button> &nbsp;
+        <Button destructive size="large">
+          Delete domain
+        </Button>{' '}
+        &nbsp;
+        <Button destructive disabled>
+          Delete domain
+        </Button>{' '}
+        &nbsp;
+      </div>
+    )),
+  )
 
-  .add('Outline', withInfo()(() => (
-    <div>
-      <Button outline size='small'>Manage IPs</Button> &nbsp;
-      <Button outline>Manage IPs</Button> &nbsp;
-      <Button outline size='large'>Manage IPs</Button> &nbsp;
-      <Button outline disabled>Manage IPs</Button> &nbsp;
-    </div>
-  )))
+  .add(
+    'Outline',
+    withInfo()(() => (
+      <div>
+        <Button outline size="small">
+          Manage IPs
+        </Button>{' '}
+        &nbsp;
+        <Button outline>Manage IPs</Button> &nbsp;
+        <Button outline size="large">
+          Manage IPs
+        </Button>{' '}
+        &nbsp;
+        <Button outline disabled>
+          Manage IPs
+        </Button>{' '}
+        &nbsp;
+      </div>
+    )),
+  )
 
-  .add('External', withInfo()(() => (
-      <Button to='http://google.com'>Google</Button>
-  )))
+  .add(
+    'External',
+    withInfo()(() => <Button to="http://google.com">Google</Button>),
+  )
 
-  .add('Group', withInfo()(() => (
+  .add(
+    'Group',
+    withInfo()(() => (
       <Button.Group>
         <Button>Linear</Button>
         <Button>Log</Button>
         <Button disabled>Sq Rt</Button>
       </Button.Group>
-  )));
+    )),
+  );
