0e850cc03c78fb18e18df556c17bacddf36e564f
Jon Ambas <jon@jonambas.com>
2020-01-23 09:29:05 -0500

    FE-793 Add Inline Component (#316)
    * Upgrade Storybook to 5.x
    
    * Remove usage of default token variants (#277)
    
    * Upgrade design tokens and remove token default instances
    
    * Upgrade design tokens in matchbox
    
    * Fix a design token mock in Text test
    
    * Update unreleased.md
    
    * Custom Deprecate Prop Type (#280)
    
    * Add custom deprecate prop type
    
    * Update unreleased.md
    
    * Fix a typo in unreleased.md
    
    * FE-793 Add Inline component with space prop
    
    * FE-793 Fix responsive prop index selection
    
    * FE-793 Update themeprovider snapshot
    
    * FE-793 Add align prop
    
    * FE-793 Add styled-system util packages to build
    
    * FE-793 Add missing Stack export in component index file
    
    * FE-793 Use forEach instead of for in, on an array
    
    * FE-793 Remove unecessary flex property
    
    * FE-793 Use styled-system utils instead of custom transforming css
    
    * FE-793 Use overflow auto instead of 1px padding, Remove fragment render
    
    * FE-793 Add a comment about margin collapsing
    
    * FE-793 Resolve rebase conflicts
    
    * FE-793 Remove ignored file

---
+++ stories/layout/Inline.stories.js
@@ -0,0 +1,91 @@
+import React from "react";
+import { addDecorator } from "@storybook/react";
+import { withInfo } from "@storybook/addon-info";
+
+import { Inline } from "@sparkpost/matchbox/components/Inline";
+import { Box, Text } from "@sparkpost/matchbox/components";
+import { ThemeProvider } from "@sparkpost/matchbox/components/ThemeProvider";
+
+addDecorator(storyFn => <ThemeProvider>{storyFn()}</ThemeProvider>);
+
+export default {
+  title: "Layout|Inline"
+};
+
+export const Spacing = withInfo({ propTables: [Inline] })(() => (
+  <div>
+    <Text m="500">Spacing:</Text>
+    <Box
+      m="500"
+      width="260px"
+      bg="green.200"
+    >
+      <Inline space="200">
+        <Box bg="blue.500" height="50px" width="50px" />
+        <Box bg="blue.500" height="50px" width="50px" />
+        <Box bg="blue.500" height="50px" width="50px" />
+        <Box bg="blue.500" height="50px" width="80px" />
+        <Box bg="blue.500" height="50px" width="50px" />
+        <Box bg="blue.500" height="50px" width="50px" />
+        <Box bg="blue.500" height="50px" width="50px" />
+        <Box bg="blue.500" height="50px" width="50px" />
+      </Inline>
+    </Box>
+    <Text m="500">Responsive Spacing:</Text>
+    <Box
+      m="500"
+      width="260px"
+      bg="green.200"
+    >
+      <Inline space={["200", "400", "500", "50px"]}>
+        <Box bg="blue.500" height="50px" width="50px" />
+        <Box bg="blue.500" height="50px" width="50px" />
+        <Box bg="blue.500" height="50px" width="50px" />
+        <Box bg="blue.500" height="50px" width="80px" />
+        <Box bg="blue.500" height="50px" width="50px" />
+        <Box bg="blue.500" height="50px" width="50px" />
+        <Box bg="blue.500" height="50px" width="50px" />
+        <Box bg="blue.500" height="50px" width="50px" />
+      </Inline>
+    </Box>
+  </div>
+));
+
+export const Alignment = withInfo({ propTables: [Inline] })(() => (
+  <div>
+    <Text m="500">Alignment:</Text>
+    <Box
+      m="500"
+      width="260px"
+      bg="green.200"
+    >
+      <Inline align='center'>
+        <Box bg="blue.500" height="50px" width="50px" />
+        <Box bg="blue.500" height="50px" width="50px" />
+        <Box bg="blue.500" height="50px" width="50px" />
+        <Box bg="blue.500" height="50px" width="80px" />
+        <Box bg="blue.500" height="50px" width="50px" />
+        <Box bg="blue.500" height="50px" width="50px" />
+        <Box bg="blue.500" height="50px" width="50px" />
+        <Box bg="blue.500" height="50px" width="50px" />
+      </Inline>
+    </Box>
+    <Text m="500">Responsive Alignment:</Text>
+    <Box
+      m="500"
+      width="260px"
+      bg="green.200"
+    >
+      <Inline align={['left', 'center', 'right', 'center']}>
+        <Box bg="blue.500" height="50px" width="50px" />
+        <Box bg="blue.500" height="50px" width="50px" />
+        <Box bg="blue.500" height="50px" width="50px" />
+        <Box bg="blue.500" height="50px" width="80px" />
+        <Box bg="blue.500" height="50px" width="50px" />
+        <Box bg="blue.500" height="50px" width="50px" />
+        <Box bg="blue.500" height="50px" width="50px" />
+        <Box bg="blue.500" height="50px" width="50px" />
+      </Inline>
+    </Box>
+  </div>
+));
--- unreleased.md
+++ unreleased.md
@@ -3,6 +3,7 @@
 - #262 - Resolve issue #261 by removing padding right styles from Panel.Section content
 
 [4.0.0]
+
 - #267 - Updates to Babel 7, Rollup 1, Storybook 5
 - #270 - Refreshes global CSS, Adds Box component, and Installs styled-system
 - #272 - Adds primitive Text component
@@ -10,3 +11,4 @@
 - #278 - Adds @sparkpost/design-tokens to the Matchbox monorepo
 - #280 - Adds a custom deprecate prop type
 - #308 - Adds layout Stack component
+- #316 - Adds layout Inline component
