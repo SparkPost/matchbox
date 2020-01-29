4b32e4b07bbb26e5db168dbf826b491c5ceac67d
Logan Sparlin <logan.sparlin@gmail.com>
2020-01-29 09:22:34 -0500

    ProgressBar Changes

--- packages/matchbox/src/components/ProgressBar/ProgressBar.js
+++ packages/matchbox/src/components/ProgressBar/ProgressBar.js
@@ -1,45 +1,42 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
+import { deprecate } from '../../helpers/propTypes';
 import classnames from 'classnames';
 import styles from './ProgressBar.module.scss';
 
-class ProgressBar extends Component {
-  static displayName = 'ProgressBar';
+function ProgressBar(props) {
+  const { completed = 0, size } = props;
 
-  static propTypes = {
-    /**
-     * Completion in percentage
-     */
-    completed: PropTypes.number.isRequired,
+  let percentage = completed;
 
-    /**
-     * Bar color
-     */
-    color: PropTypes.oneOf(['orange', 'blue', 'navy', 'purple', 'red'])
-  };
-
-  static defaultProps = {
-    completed: 0,
-    color: 'orange'
+  if (percentage > 100) {
+    percentage = 100;
+  } else if (percentage < 1) {
+    percentage = 0;
   }
 
-  render() {
-    const { completed = 0, color } = this.props;
+  const classname = classnames(styles.ProgressBar, styles['blue'], styles[size]);
 
-    let percentage = completed;
-
-    if (percentage > 100) {
-      percentage = 100;
-    } else if (percentage < 1) {
-      percentage = 0;
-    }
-
-    return (
-      <div className={classnames(styles.ProgressBar, styles[color])}>
-        <div className={styles.Progress} style={{ width: `${percentage}%` }}/>
-      </div>
-    );
-  }
+  return (
+    <div className={classname}>
+      <div className={styles.Progress} style={{ width: `${percentage}%` }} />
+    </div>
+  );
 }
+
+ProgressBar.displayName = 'ProgressBar';
+
+ProgressBar.propTypes = {
+  completed: PropTypes.number.isRequired,
+  color: deprecate(
+    PropTypes.oneOf(['orange', 'blue', 'navy', 'purple', 'red']),
+    'Color is always blue for now. This may be updated in the future.',
+  ),
+  size: PropTypes.oneOf(['normal', 'small']),
+};
+
+ProgressBar.defaultProps = {
+  size: 'normal',
+};
 
 export default ProgressBar;
--- stories/feedback/ProgressBar.stories.js
+++ stories/feedback/ProgressBar.stories.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
-import { action } from '@storybook/addon-actions';
 import StoryContainer from '../storyHelpers/StoryContainer';
 
 import { ProgressBar } from '@sparkpost/matchbox';
 
 storiesOf('Feedback|ProgressBar', module)
-  .addDecorator((getStory) => (
-    <StoryContainer>{ getStory() }</StoryContainer>
-  ))
-  .add('Default', withInfo()(() => (
-    <ProgressBar completed={54} color='red' />
-  )));
+  .addDecorator(getStory => <StoryContainer>{getStory()}</StoryContainer>)
+  .add(
+    'Default',
+    withInfo()(() => (
+      <div>
+        <ProgressBar completed={54} color="purple" />
+        <ProgressBar completed={82} size="small" />
+      </div>
+    )),
+  );
