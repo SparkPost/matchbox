bfcff6ca69f51439437b037326cbf388dcc736c8
Jon Ambas <jon@jonambas.com>
2020-02-10 16:13:34 -0500

    FE-795 Restyle Snackbar

---
+++ packages/matchbox/src/components/Snackbar/styles.js
@@ -0,0 +1,59 @@
+import { tokens } from '@sparkpost/design-tokens';
+
+export const base = () => `
+  display: inline-flex;
+  align-items: flex-start;
+  color: white;
+`;
+
+export const status = props => {
+  switch (props.status) {
+    case 'success':
+      return `background: ${tokens.color_green_800};`;
+    case 'danger':
+    case 'error':
+      return `background: ${tokens.color_red_700};`;
+    case 'warning':
+      return `
+        background: ${tokens.color_yellow_300};
+        color: ${tokens.color_gray_900};
+      `;
+    case 'default':
+    default:
+      return `background: ${tokens.color_blue_800};`;
+  }
+};
+
+export const dismiss = () => `
+  display: inline-flex;
+  border: none;
+  background: transparent;
+  transition: background ${tokens.motion_duration_fast};
+`;
+
+export const dismissStatus = props => {
+  let color;
+
+  switch (props.status) {
+    case 'success':
+      color = tokens.color_green_900;
+      break;
+    case 'danger':
+    case 'error':
+      color = tokens.color_red_800;
+      break;
+    case 'warning':
+      color = tokens.color_yellow_400;
+      break;
+    case 'default':
+    default:
+      color = tokens.color_blue_900;
+      break;
+  }
+
+  return `
+    &:hover {
+      background: ${color};
+    }
+  `;
+};
--- stories/feedback/Snackbar.stories.js
+++ stories/feedback/Snackbar.stories.js
@@ -1,33 +1,43 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
+import { addDecorator } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
 import { action } from '@storybook/addon-actions';
-import StoryContainer from '../storyHelpers/StoryContainer';
 
+import { ThemeProvider } from '@sparkpost/matchbox/components/ThemeProvider';
 import { Snackbar } from '@sparkpost/matchbox';
 
+addDecorator(storyFn => <ThemeProvider>{storyFn()}</ThemeProvider>);
 
-storiesOf('Feedback|Snackbar', module)
-  .addDecorator((getStory) => (
-    <StoryContainer>{ getStory() }</StoryContainer>
-  ))
+export default {
+  title: 'Feedback|Snackbar',
+};
 
-  .add('Default', withInfo()(() => (
-    <Snackbar onDismiss={action('Dismissed')}>Template deleted</Snackbar>
-  )))
+export const Default = withInfo()(() => (
+  <Snackbar onDismiss={action('Dismissed')}>Template deleted</Snackbar>
+));
 
-  .add('Success', withInfo()(() => (
-    <Snackbar status='success' onDismiss={action('Dismissed')}>Template deleted</Snackbar>
-  )))
+export const Success = withInfo()(() => (
+  <Snackbar status="success" onDismiss={action('Dismissed')}>
+    Template deleted
+  </Snackbar>
+));
 
-  .add('Danger or Error', withInfo()(() => (
-    <Snackbar status='danger' onDismiss={action('Dismissed')}>Template deleted</Snackbar>
-  )))
+export const DangerOrError = withInfo()(() => (
+  <Snackbar status="danger" onDismiss={action('Dismissed')}>
+    Template deleted
+  </Snackbar>
+));
 
-  .add('Warning', withInfo()(() => (
-    <Snackbar status='warning' onDismiss={action('Dismissed')}>Yer suspended</Snackbar>
-  )))
+export const Warning = withInfo()(() => (
+  <Snackbar status="warning" onDismiss={action('Dismissed')}>
+    Yer suspended
+  </Snackbar>
+));
 
-  .add('Large', withInfo()(() => (
-    <Snackbar maxWidth={700}>This one is large enough to get into some bacon ipsum dolor amet pork loin tri-tip turkey capicola. Rump doner short ribs biltong burgdoggen meatloaf. Prosciutto pork loin bacon, biltong landjaeger salami ham spare ribs flank cupim porchetta leberkas.</Snackbar>
-  )));
+export const Large = withInfo()(() => (
+  <Snackbar maxWidth={700}>
+    This one is large enough to get into some bacon ipsum dolor amet pork loin tri-tip turkey
+    capicola. Rump doner short ribs biltong burgdoggen meatloaf. Prosciutto pork loin bacon, biltong
+    landjaeger salami ham spare ribs flank cupim porchetta leberkas.
+  </Snackbar>
+));
