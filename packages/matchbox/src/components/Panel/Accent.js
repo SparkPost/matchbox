66fc8e56689d8e14f32dc75bc9a0a15f694f3df9
Logan Sparlin <logan.sparlin@gmail.com>
2020-02-11 00:39:05 -0600

    Undo accidental master merge

---
+++ packages/matchbox/src/components/Panel/Accent.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import { accent } from './styles';
+
+const AccentOuter = styled('div')`
+  ${accent}
+`;
+
+const Accent = ({ accentColor }) => {
+  return <AccentOuter accentColor={accentColor} />;
+};
+
+Accent.displayName = 'Panel.Accent';
+Accent.propTypes = {
+  accentColor: PropTypes.oneOf(['orange', 'blue', 'red', 'yellow', 'green', 'gray']),
+};
+
+export default Accent;
