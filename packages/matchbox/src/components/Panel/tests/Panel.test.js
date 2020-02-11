b47a47ce5d9a947ea156b5c91b01ce2cd12fbec9
Logan Sparlin <logan.sparlin@gmail.com>
2020-02-11 16:01:09 -0600

    Add more tests

--- packages/matchbox/src/components/Panel/tests/Panel.test.js
+++ packages/matchbox/src/components/Panel/tests/Panel.test.js
@@ -4,7 +4,60 @@ import { tokens } from '@sparkpost/design-tokens';
 import 'jest-styled-components';
 
 describe('Panel', () => {
-  let wrapper;
+  let wrapper = global.mountStyled(
+    <Panel accent sectioned title="test title">
+      Panel Content
+    </Panel>,
+  );
+
+  it('renders panel styles', () => {
+    expect(wrapper).toHaveStyleRule('margin-bottom', tokens.spacing_400);
+    expect(wrapper).toHaveStyleRule('background', tokens.color_white);
+  });
+
+  it('renders header styles', () => {
+    expect(wrapper.find(Panel.Header)).toHaveStyleRule('display', 'flex');
+    expect(wrapper.find(Panel.Header)).toHaveStyleRule('font-weight', tokens.fontWeight_semibold);
+    expect(wrapper.find(Panel.Header)).toHaveStyleRule(
+      'padding',
+      `${tokens.spacing_500} ${tokens.spacing_500} 0 ${tokens.spacing_500}`,
+    );
+  });
+
+  it('renders section styles', () => {
+    wrapper = global.mountStyled(
+      <Panel accent sectioned title="test title">
+        <Panel.Section>Foo</Panel.Section>
+        <Panel.Section>Bar</Panel.Section>
+      </Panel>,
+    );
+
+    expect(wrapper.find(Panel.Section).at(0)).toHaveStyleRule('display', 'flex');
+    expect(wrapper.find(Panel.Section).at(0)).toHaveStyleRule(
+      'padding',
+      `18px ${tokens.spacing_500}`,
+    );
+    expect(wrapper.find(Panel.Section).at(0)).toHaveStyleRule(
+      'border-bottom',
+      `${tokens.borderWidth_100} solid ${tokens.color_gray_400}`,
+    );
+    expect(wrapper.find(Panel.Section).at(1)).toHaveStyleRule('border-bottom', 'none', {
+      modifier: ':last-of-type',
+    });
+  });
+
+  it('renders footer styles', () => {
+    wrapper = global.mountStyled(
+      <div>
+        <Panel accent sectioned title="test title">
+          Foo
+        </Panel>
+        <Panel.Footer left={<span>left</span>} right="right" />
+      </div>,
+    );
+
+    expect(wrapper.find(Panel.Footer)).toHaveStyleRule('margin-top', `-${tokens.spacing_300}`);
+  });
 
   it('renders accent title and header action correctly', () => {
     wrapper = global.mountStyled(
