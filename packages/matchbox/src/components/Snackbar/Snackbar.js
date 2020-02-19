b1b0f775f899023e3fb8fac9bdd4867be7942c26
Jon Ambas <jon@jonambas.com>
2020-02-19 11:19:51 -0500

    FE-795 Update Snackbar (#328)
    * FE-795 Restyle Snackbar
    
    * FE-795 Add margin system prop
    
    * FE-795 Update Snackbar unit tests
    
    * FE-795 Update unreleased.md
    
    * FE-795 Consolidate snackbar stories
    
    * FE-795 Use color white token and Fix motion duration token name

--- packages/matchbox/src/components/Snackbar/Snackbar.js
+++ packages/matchbox/src/components/Snackbar/Snackbar.js
@@ -1,87 +1,72 @@
-import React, { Component } from 'react';
+import React from 'react';
+import styled from 'styled-components';
+import { Box } from '../';
+import { ScreenReaderOnly } from '../ScreenReaderOnly';
 import PropTypes from 'prop-types';
-import classnames from 'classnames';
+import { createPropTypes } from '@styled-system/prop-types';
 import { Close, Info, CheckCircle, Warning, ErrorIcon } from '@sparkpost/matchbox-icons';
-import { onKey } from '../../helpers/keyEvents';
+import { margin } from 'styled-system';
+import { base, status, dismiss, dismissStatus } from './styles';
 
-import styles from './Snackbar.module.scss';
+const StyledBox = styled(Box)`
+  ${base}
+  ${status}
+  ${margin}
+`;
 
-class Snackbar extends Component {
-  static displayName = 'Snackbar';
+const StyledClose = styled(Box)`
+  ${dismiss}
+  ${dismissStatus}
+`;
 
-  static propTypes = {
-    /**
-     * The type of snackbar.
-     */
-    status: PropTypes.oneOf(['default', 'success', 'danger', 'error', 'warning']),
+function Snackbar(props) {
+  const { children, status, maxWidth, onDismiss, ...rest } = props;
 
-    /**
-     * Snackbar max-width in pixels.
-     */
-    maxWidth: PropTypes.number,
-
-    /**
-     * Callback when dismiss button is clicked.
-     */
-    onDismiss: PropTypes.func.isRequired,
-
-    /**
-     * Snackbar Content
-     */
-    children: PropTypes.node
-  };
-
-  static defaultProps = {
-    status: 'default',
-    maxWidth: 380
-  }
-
-  handleKeydown = (e) => {
-    const { onDismiss } = this.props;
-
-    onKey('space', () => onDismiss())(e);
-  }
-
-  render() {
-    const {
-      children,
-      status,
-      maxWidth,
-      onDismiss,
-      ...rest
-    } = this.props;
-
-    const snackbarStyles = classnames(
-      styles.Snackbar,
-      styles[`${status}`]
-    );
-
-    return (
-      <div className={snackbarStyles} role="alert" {...rest}>
-        <div className={styles.IconWrapper}>
-          {status === 'default' && <Info label="Info" />}
-
-          {status === 'success' && <CheckCircle label="Success"/>}
-
-          {status === 'warning' && <Warning label="Warning" />}
-
-          {(status === 'error' || status === 'danger') && <ErrorIcon label="Error" />}
-        </div>
-
-        <div className={styles.Content} style={{ maxWidth }}>{children}</div>
-
-        <a
-          className={styles.Dismiss}
-          onClick={onDismiss}
-          onKeyDown={this.handleKeydown}
-          role="button"
-          href="javascript:void(0);"
-        >
-          <Close size={21} className={styles.DismissIcon} />
-        </a>
-      </div>
-    );
-  }
+  return (
+    <StyledBox status={status} role="alert" p="300" borderRadius="100" {...rest}>
+      <Box mr="300">
+        {status === 'default' && <Info size="20" label="Info" />}
+        {status === 'success' && <CheckCircle size="20" label="Success" />}
+        {status === 'warning' && <Warning size="20" label="Warning" />}
+        {(status === 'error' || status === 'danger') && <ErrorIcon size="20" label="Error" />}
+      </Box>
+      <Box fontSize="200" lineHeight="200" py="100" pr="400" maxWidth={maxWidth} minWidth="12.5rem">
+        {children}
+      </Box>
+      <StyledClose status={status} as="button" p="0.125rem" mx="100" onClick={onDismiss}>
+        <Close size={24} />
+        <ScreenReaderOnly>Close</ScreenReaderOnly>
+      </StyledClose>
+    </StyledBox>
+  );
 }
+
+Snackbar.displayName = 'Snackbar';
+Snackbar.defaultProps = {
+  status: 'default',
+  maxWidth: 380,
+};
+Snackbar.propTypes = {
+  /**
+   * The type of snackbar.
+   */
+  status: PropTypes.oneOf(['default', 'success', 'danger', 'error', 'warning']),
+
+  /**
+   * Snackbar max-width in pixels.
+   */
+  maxWidth: PropTypes.number,
+
+  /**
+   * Callback when dismiss button is clicked.
+   */
+  onDismiss: PropTypes.func.isRequired,
+
+  /**
+   * Snackbar Content
+   */
+  children: PropTypes.node,
+  ...createPropTypes(margin.propNames),
+};
 
 export default Snackbar;
--- unreleased.md
+++ unreleased.md
@@ -20,3 +20,4 @@
   only content
 - #324 - Restyles the Tag component
 - #324 - Tag component close button is now a button, and handles both space and enter events
+- #328 - Snackbar dismiss button is now a button, and handles both space and enter events
