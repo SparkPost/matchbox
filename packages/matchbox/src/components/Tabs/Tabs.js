d8158925e08e49cd1639b7f90910a038e95f9e11
Jon Ambas <jon@jonambas.com>
2020-03-06 17:25:00 -0500

    FE-822 Start tabs

--- packages/matchbox/src/components/Tabs/Tabs.js
+++ packages/matchbox/src/components/Tabs/Tabs.js
@@ -1,71 +1,76 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import classnames from 'classnames';
-
+import styled from 'styled-components';
+import { tokens } from '@sparkpost/design-tokens';
 import { UnstyledLink } from '../UnstyledLink';
+import { Box } from '../Box';
+import { deprecate } from '../../helpers/propTypes';
+import { buttonReset } from '../../styles/helpers';
 
-import styles from './Tabs.module.scss';
+const StyledTab = styled(Box)`
+  ${buttonReset}
+  ${'' /* Not a token, equivalent to 20px to enqure 10rem of spacing between text */}
+  padding: 0 1.25rem;
+  line-height: 3.75rem;
 
-class Tab extends Component {
-  static displayName = 'Tab';
+  &:after {
+    display: block;
+    position: absolute;
+    content: '';
+    bottom: -1px;
+    left: 0;
+    right: 0;
+    height: ${tokens.spacing_100};
+    transition: ${tokens.motionDuration_fast} ${tokens.motionEase_in_out};
+  }
 
-  handleClick = (event) => {
-    const { index, onClick } = this.props;
+  ${({ isSelected }) => `
+    color: ${isSelected ? tokens.color_blue_700 : tokens.color_gray_700};
+    
+    &:after {
+      background: ${isSelected ? tokens.color_blue_700 : 'transparent'};
+    }
+
+    &:hover {
+      ${!isSelected ? `color: ${tokens.color_gray_900};` : ''};
+      &:after {
+        ${!isSelected ? `background: ${tokens.color_gray_400};` : ''}
+      }
+    }
+  `}
+`;
+
+function Tab(props) {
+  const { index, content, selected, fitted, ...rest } = props;
+
+  function handleClick(event) {
+    const { index, onClick } = props;
     onClick(event, index);
   }
 
-  render() {
-    const { index, content, selected, fittedTab, ...rest } = this.props;
-    const classes = classnames(
-      styles.Tab,
-      selected === index && styles.selected,
-      fittedTab && styles.fittedTab
-    );
-
-    return (
-      <UnstyledLink className={classes} {...rest} onClick={this.handleClick}>
-        {content}
-      </UnstyledLink>
-    );
-  }
+  return (
+    <StyledTab
+      as={UnstyledLink}
+      component="button" // Ensures focusability
+      position="relative"
+      flex={fitted ? '1' : '0'}
+      isSelected={selected === index}
+      fontSize="200"
+      fontWeight="medium"
+      {...rest}
+      onClick={handleClick}
+    >
+      {content}
+    </StyledTab>
+  );
 }
 
-class Tabs extends Component {
-  static displayName = 'Tabs';
+Tab.displayName = 'Tab';
 
-  static propTypes = {
-    /**
-     * Tab Content
-     * Actions that build the tabs. Most button and unstyled link props will work in here.
-     * e.g. { content: 'Label', onClick: callback() }
-     */
-    tabs: PropTypes.arrayOf(PropTypes.shape({
-      content: PropTypes.node.isRequired
-    })),
+function Tabs(props) {
+  const { tabs, selected, onSelect, connectBelow, fitted } = props;
 
-    /**
-     * Tab Color
-     */
-    color: PropTypes.oneOf(['orange', 'blue', 'navy', 'purple', 'red']),
-    /**
-      * Index of selected tab
-      */
-    selected: PropTypes.number.isRequired,
-    /**
-      * Connects this component with component underneath it. Works well with Panels.
-      */
-    connectBelow: PropTypes.bool,
-
-    onSelect: PropTypes.func
-  };
-
-  static defaultProps = {
-    connectBelow: true,
-    color: 'orange'
-  };
-
-  handleClick = (event, index) => {
-    const { onSelect, selected, tabs } = this.props;
+  function handleClick(event, index) {
     const { onClick } = tabs[index];
 
     if (onClick) {
@@ -77,26 +82,50 @@ class Tabs extends Component {
     }
   }
 
-  render() {
-    const { tabs, selected, connectBelow, color, fitted } = this.props;
+  // const tabsClasses = classnames(
+  //   styles.Tabs,
+  //   fitted && styles.fitted
+  // );
 
-    const tabMarkup = tabs.map((tab, i) => (
-      <Tab key={i} index={i} fittedTab={fitted} selected={selected} {...tab} onClick={this.handleClick} />
-    ));
-
-    const tabsClasses = classnames(
-      styles.Tabs,
-      styles[`color-${color}`],
-      connectBelow && styles.connectBelow,
-      fitted && styles.fitted
-    );
-
-    return (
-      <div className={tabsClasses}>
-        {tabMarkup}
-      </div>
-    );
-  }
+  return (
+    <Box display="flex" borderBottom="400" mb={connectBelow ? '0' : '400'}>
+      {tabs.map((tab, i) => (
+        <Tab key={i} index={i} fitted={fitted} selected={selected} {...tab} onClick={handleClick} />
+      ))}
+    </Box>
+  );
 }
+
+Tabs.displayName = 'Tabs';
+Tabs.propTypes = {
+  /**
+   * Tab Content
+   * Actions that build the tabs. Most button and unstyled link props will work in here.
+   * e.g. { content: 'Label', onClick: callback() }
+   */
+  tabs: PropTypes.arrayOf(
+    PropTypes.shape({
+      content: PropTypes.node.isRequired,
+    }),
+  ),
+
+  /**
+   * Tab Color
+   */
+  color: deprecate(
+    PropTypes.oneOf(['orange', 'blue', 'navy', 'purple', 'red']),
+    'Tab color is no longer configurable',
+  ),
+  /**
+   * Index of selected tab
+   */
+  selected: PropTypes.number.isRequired,
+  /**
+   * Connects this component with component underneath it. Works well with Panels.
+   */
+  connectBelow: deprecate(PropTypes.bool, 'Deprecated in favor of margin system props'),
+
+  onSelect: PropTypes.func,
+};
 
 export default Tabs;
--- stories/navigation/Tabs.stories.js
+++ stories/navigation/Tabs.stories.js
@@ -1,76 +1,95 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
+import { addDecorator } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
 import { action } from '@storybook/addon-actions';
-import StoryContainer from '../storyHelpers/StoryContainer';
+// import StoryContainer from '../storyHelpers/StoryContainer';
 
-import { Tabs, Page, Panel } from '@sparkpost/matchbox';
+import { Tabs, ThemeProvider } from '@sparkpost/matchbox';
 
-const secondaryActions = [
-  {
-    content: 'Save',
-    onClick: action('Save Clicked')
-  },
-  {
-    content: 'Delete',
-    onClick: action('Delete Clicked')
-  }
-];
-
-const breadcrumbAction = {
-  content: 'Webhooks',
-  onClick: action('Webhooks Clicked')
-}
+addDecorator(storyFn => <ThemeProvider>{storyFn()}</ThemeProvider>);
 
 const tabs = [
   {
     content: 'Details',
-    onClick: action('Details Clicked')
+    onClick: action('Details Clicked'),
   },
   {
     content: 'Keys',
-    onClick: action('Keys Clicked')
+    onClick: action('Keys Clicked'),
   },
   {
     content: 'Domains',
-    onClick: action('Domains Clicked')
+    onClick: action('Domains Clicked'),
   },
 ];
 
-const handleSelect = action('Tab Selected');
+// const handleSelect = action('Tab Selected');
 
-storiesOf('Navigation|Tabs', module)
-  .addDecorator((getStory) => (
-    <StoryContainer>{ getStory() }</StoryContainer>
-  ))
-  .add('basic example', withInfo()(() => (
-    <Tabs selected={0} connectBelow={false} onSelect={handleSelect} tabs={tabs}/>
-  )))
+const Example = () => {
+  const [i, seti] = React.useState(0);
+  return <Tabs selected={i} onSelect={ind => seti(ind)} tabs={tabs} />;
+};
 
-  .add('fitted tabs', withInfo()(() => (
-    <Tabs fitted selected={0} connectBelow={false} onSelect={handleSelect} tabs={tabs}/>
-  )))
+export default {
+  title: 'Navigation|Tabs',
+};
 
-  .add('with other components', withInfo({
-    propTablesExclude: [Page, Panel]
-  })(() => (
-    <div>
-      <Page
-        secondaryActions={secondaryActions}
-        breadcrumbAction={breadcrumbAction}
-        title='Webhook #2'
-      />
-      <Tabs selected={0} color='red' onSelect={handleSelect} tabs={tabs} />
-      <Panel sectioned>A panel</Panel>
-    </div>
-  )))
+export const DefaultTabs = withInfo()(() => <Example />);
 
-  .add('colors', withInfo()(() => (
-    <div>
-      <Tabs connectBelow={false} selected={0} color='purple' onSelect={handleSelect} tabs={tabs} />
-      <Tabs connectBelow={false} selected={0} color='navy' onSelect={handleSelect} tabs={tabs} />
-      <Tabs connectBelow={false} selected={0} color='blue' onSelect={handleSelect} tabs={tabs} />
-      <Tabs connectBelow={false} selected={0} color='orange' onSelect={handleSelect} tabs={tabs} />
-      <Tabs connectBelow={false} selected={0} color='red' onSelect={handleSelect} tabs={tabs} />
-    </div>
-  )));
+// storiesOf('Navigation|Tabs', module)
+//   .addDecorator(getStory => <StoryContainer>{getStory()}</StoryContainer>)
+//   .add(
+//     'basic example',
+//     withInfo()(() => (
+//       <Tabs selected={0} connectBelow={false} onSelect={handleSelect} tabs={tabs} />
+//     )),
+//   )
+
+//   .add(
+//     'fitted tabs',
+//     withInfo()(() => (
+//       <Tabs fitted selected={0} connectBelow={false} onSelect={handleSelect} tabs={tabs} />
+//     )),
+//   )
+
+//   .add(
+//     'with other components',
+//     withInfo({
+//       propTablesExclude: [Page, Panel],
+//     })(() => (
+//       <div>
+//         <Page
+//           secondaryActions={secondaryActions}
+//           breadcrumbAction={breadcrumbAction}
+//           title="Webhook #2"
+//         />
+//         <Tabs selected={0} color="red" onSelect={handleSelect} tabs={tabs} />
+//         <Panel sectioned>A panel</Panel>
+//       </div>
+//     )),
+//   )
+
+//   .add(
+//     'colors',
+//     withInfo()(() => (
+//       <div>
+//         <Tabs
+//           connectBelow={false}
+//           selected={0}
+//           color="purple"
+//           onSelect={handleSelect}
+//           tabs={tabs}
+//         />
+//         <Tabs connectBelow={false} selected={0} color="navy" onSelect={handleSelect} tabs={tabs} />
+//         <Tabs connectBelow={false} selected={0} color="blue" onSelect={handleSelect} tabs={tabs} />
+//         <Tabs
+//           connectBelow={false}
+//           selected={0}
+//           color="orange"
+//           onSelect={handleSelect}
+//           tabs={tabs}
+//         />
+//         <Tabs connectBelow={false} selected={0} color="red" onSelect={handleSelect} tabs={tabs} />
+//       </div>
+//     )),
+//   );
