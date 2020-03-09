ebc2fbbd1168f29935189dde9b0a341ed3b35fa0
Jon Ambas <jon@jonambas.com>
2020-03-09 18:46:04 -0400

    FE-913 Update skipGlobalStyles prop

--- packages/matchbox/src/components/ThemeProvider/ThemeProvider.js
+++ packages/matchbox/src/components/ThemeProvider/ThemeProvider.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { ThemeProvider, createGlobalStyle } from 'styled-components';
 import { normalize } from 'styled-normalize';
 import theme from './theme';
@@ -22,14 +23,19 @@ function Theme(props) {
   return (
     <ThemeProvider theme={{ ...theme, ...props.theme }}>
       {/* 
-        FE-913: `skipGlobalStyles` is to exclude global styles from unit tests
-        The custom matcher `toHaveAttributeValue` fails with this turned on
-        `TypeError: received.getDOMNode(...).getAttribute is not a function`
+        FE-913:`skipGlobalStyles` is to exclude global styles from unit tests.
+        The custom assertion `toHaveAttributeValue` fails with this turned on.
+        We should either move away from using this assertion (or even enzyme),
+        or try to resolve the error
       */}
       {!props.skipGlobalStyles && <GlobalStyle />}
       {props.children}
     </ThemeProvider>
   );
 }
+
+Theme.propTypes = {
+  skipGlobalStyles: PropTypes.bool,
+};
 
 export default Theme;
