ad0c8663ef69a5dca558c923e28c8e080b702f04
Logan Sparlin <logan.sparlin@gmail.com>
2020-03-11 09:31:52 -0500

    FE-823 Update Pager Component (#343)
    * Update Pager Component
    
    * Update unreleased.md
    
    * Add button prop and system prop stories
    
    * fix test
    
    * change styled Box to div
    
    * move display inline-block into styled declaration

--- packages/matchbox/src/components/Pager/tests/Pager.test.js
+++ packages/matchbox/src/components/Pager/tests/Pager.test.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { tokens } from '@sparkpost/design-tokens';
 import Pager from '../Pager';
+import 'jest-styled-components';
 
 describe('Pager', () => {
-  it('renders pager with buttons', () => {
-    const wrapper = shallow(
-      <Pager>
-        <Pager.Previous />
-        <Pager.Next />
-      </Pager>
-    );
+  const wrapper = global.mountStyled(
+    <Pager mb="400">
+      <Pager.Previous />
+      <Pager.Next />
+    </Pager>,
+  );
 
-    expect(wrapper).toMatchSnapshot();
+  it('renders pager with buttons', () => {
+    expect(wrapper.find(Pager.Previous)).toExist();
+    expect(wrapper.find(Pager.Next)).toExist();
+  });
+
+  it('renders pager styles', () => {
+    expect(wrapper).toHaveStyleRule('display', 'inline-block');
+    expect(wrapper).toHaveStyleRule('margin-bottom', tokens.spacing_400);
   });
 });
