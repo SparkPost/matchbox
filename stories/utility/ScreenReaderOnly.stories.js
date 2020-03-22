e7d10a2a9627ae3952d3785968cc71775c307a67
Nick Lemmon <nick.lemmon@sparkpost.com>
2020-03-22 14:06:30 -0400

    FE-919 - Format file with Prettier

--- stories/utility/ScreenReaderOnly.stories.js
+++ stories/utility/ScreenReaderOnly.stories.js
@@ -5,15 +5,17 @@ import StoryContainer from '../storyHelpers/StoryContainer';
 import { ScreenReaderOnly } from '@sparkpost/matchbox';
 
 storiesOf('Utility|ScreenReaderOnly', module)
-  .addDecorator((getStory) => (
-    <StoryContainer>{getStory()}</StoryContainer>
-  ))
-  .add('basic example', withInfo('This component hides content from sighted users, exposing it only to screen reader users.')(() => (
-    <p>
-      I am content that is available to all users...
-
-      <ScreenReaderOnly>
-        ...and I am content that is only available to screen reader users.
-      </ScreenReaderOnly>
-    </p>
-  )));
+  .addDecorator(getStory => <StoryContainer>{getStory()}</StoryContainer>)
+  .add(
+    'basic example',
+    withInfo(
+      'This component hides content from sighted users, exposing it only to screen reader users.',
+    )(() => (
+      <p>
+        I am content that is available to all users...
+        <ScreenReaderOnly>
+          ...and I am content that is only available to screen reader users.
+        </ScreenReaderOnly>
+      </p>
+    )),
+  );
