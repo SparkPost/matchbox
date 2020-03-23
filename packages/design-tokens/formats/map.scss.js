5740119f52029f781fb4459a6cfbc691f912c07c
Jon Ambas <jon@jonambas.com>
2020-03-23 17:28:50 -0400

    FE-941 Add token unit tests, Add root font size token

--- packages/design-tokens/formats/map.scss.js
+++ packages/design-tokens/formats/map.scss.js
@@ -2,16 +2,16 @@ const path = require('path');
 const { mapGet, typographyMapGet } = require('./templates');
 
 function map(result) {
-  const { props, meta, aliases } = result.toJS();
+  const { props, meta } = result.toJS();
   const basename = path.basename(meta.file, '.yml');
 
-  const renderProp = (prop) => `${prop.name}: (${prop.value})`;
+  const renderProp = prop => `${prop.name}: (${prop.value})`;
 
   return `
     $${basename}-map: (
       ${props.map(renderProp)}
     );
-    ${(basename === 'font-size' ? typographyMapGet : mapGet)(basename, aliases)}
+    ${(basename === 'font-size' ? typographyMapGet : mapGet)(basename, props)}
   `;
 }
 
