652334fd982981de31a84c2a944db1014ac7b51c
Jon Ambas <jon@jonambas.com>
2020-03-24 12:43:32 -0400

    FE-810 Update ActionList (#358)
    * FE-810 Update ActionList
    
    * FE-810 Add Action propTypes and Update unreleased.md
    
    * FE-810 Support buttons and checkboxes as ActionList actions
    
    * FE-810 Add Actionlist system prop story and Add links to all actions

--- packages/matchbox/src/components/ActionList/tests/ActionList.test.js
+++ packages/matchbox/src/components/ActionList/tests/ActionList.test.js
@@ -1,51 +1,92 @@
 import React from 'react';
 import ActionList from '../ActionList';
-import { shallow } from 'enzyme';
+import 'jest-styled-components';
 
 describe('ActionList', () => {
+  const subject = props => global.mountStyled(<ActionList {...props} />);
+
   it('renders actions correctly', () => {
-    const wrapper = shallow(
-      <ActionList actions={[{ content: 'Action' }, { content: 'Action 2', selected: true }]} />
-    );
-    expect(wrapper).toMatchSnapshot();
-    expect(wrapper.find('Section').dive()).toMatchSnapshot();
+    const wrapper = subject({
+      actions: [{ content: 'Action' }, { content: 'Action 2' }],
+    });
+    expect(
+      wrapper
+        .find('a')
+        .at(0)
+        .text(),
+    ).toEqual('Action');
   });
 
   it('renders sections correctly', () => {
-    const wrapper = shallow(
-      <ActionList
-        sections={[
-          [{ content: 'Sectioned1' }, { content: 'Sectioned2' }],
-          [{ content: 'Sectioned3' }, { content: 'Sectioned4' }]
-        ]}
-      />
-    );
-    expect(wrapper).toMatchSnapshot();
+    const wrapper = subject({
+      sections: [
+        [{ content: 'Sectioned1' }, { content: 'Sectioned2' }],
+        [{ content: 'Sectioned3' }, { content: 'Sectioned4' }],
+      ],
+    });
+    expect(
+      wrapper
+        .find('div')
+        .at(1)
+        .text(),
+    ).toEqual('Sectioned1Sectioned2');
+    expect(
+      wrapper
+        .find('div')
+        .at(2)
+        .text(),
+    ).toEqual('Sectioned3Sectioned4');
   });
 
   it('renders sections correctly using a group by key', () => {
-    const wrapper = shallow(
-      <ActionList
-        actions={[
-          { content: 'Sectioned1', section: 1 }, { content: 'Sectioned2', section: 1 },
-          { content: 'Sectioned3', section: 2 }, { content: 'Sectioned4', section: 2 }
-        ]}
-      />
-    );
-    expect(wrapper).toMatchSnapshot();
+    const wrapper = subject({
+      groupByKey: 'groupBy',
+      actions: [
+        { content: 'Sectioned1', groupBy: 1 },
+        { content: 'Sectioned2', groupBy: 1 },
+        { content: 'Sectioned3', groupBy: 2 },
+        { content: 'Sectioned4', groupBy: 4 },
+      ],
+    });
+    expect(
+      wrapper
+        .find('div')
+        .at(2)
+        .text(),
+    ).toEqual('Sectioned3');
   });
 
-  it('renders sections with actions and max height correctly', () => {
-    const wrapper = shallow(
-      <ActionList
-        maxHeight={50}
-        actions={[{ content: 'Action1' }, { content: 'Action2' }]}
-        sections={[
-          [{ content: 'Sectioned1' }, { content: 'Sectioned2' }],
-          [{ content: 'Sectioned3' }, { content: 'Sectioned4' }]
-        ]}
-      />
-    );
-    expect(wrapper).toMatchSnapshot();
+  it('renders a max height system prop as a number', () => {
+    const wrapper = subject({
+      maxHeight: 100,
+    });
+    expect(wrapper.find('div').at(0)).toHaveStyleRule('max-height', '100px');
+  });
+
+  it('renders a max height system prop as a string', () => {
+    const wrapper = subject({
+      maxHeight: '10rem',
+    });
+    expect(wrapper.find('div').at(0)).toHaveStyleRule('max-height', '10rem');
+  });
+
+  it('renders a selected action as a checkbox', () => {
+    const wrapper = subject({
+      actions: [
+        { content: 'Action', is: 'checkbox' },
+        { content: 'Action 2', selected: true, is: 'checkbox' },
+      ],
+    });
+    expect(wrapper.find('a').at(1)).toHaveAttributeValue('role', 'checkbox');
+    expect(wrapper.find('a').at(1)).toHaveAttributeValue('aria-checked', 'true');
+    expect(wrapper.find('a').at(0)).toHaveAttributeValue('aria-checked', 'false');
+  });
+
+  it('renders an action button', () => {
+    const wrapper = subject({
+      actions: [{ content: 'Action', is: 'button' }],
+    });
+    expect(wrapper.find('button').text()).toEqual('Action');
+    expect(wrapper.find('button')).toHaveAttributeValue('type', 'button');
   });
 });
