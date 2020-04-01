e41338b9bb76496f2d2a0aeb95e440e66fc18fc0
Nick Lemmon <nick.lemmon@sparkpost.com>
2020-04-01 14:15:31 -0400

    FE-825 - Update tests to match new implementation

--- packages/matchbox/src/components/Modal/tests/Modal.test.js
+++ packages/matchbox/src/components/Modal/tests/Modal.test.js
@@ -2,21 +2,29 @@ import React from 'react';
 import 'jest-styled-components';
 import Modal from '../Modal';
 
-const subject = props => global.mountStyled(<Modal {...props} />);
+const subject = props =>
+  global.mountStyled(
+    <Modal {...props}>
+      <div data-id="child-content">I am some child content</div>
+    </Modal>,
+  );
 
 describe('Modal', () => {
-  it('renders as hidden when the "open" prop is false', () => {
+  const modalPortal = global.document.createElement('div');
+  modalPortal.setAttribute('id', 'modal-portal');
+  const body = global.document.querySelector('body');
+  body.appendChild(modalPortal);
+
+  it('renders content as hidden when the "open" prop is false', () => {
     const wrapper = subject({ open: false });
 
-    expect(wrapper.find('[role="dialog"]')).toHaveStyleRule('opacity', '0');
-    expect(wrapper.find('[role="dialog"]')).toHaveStyleRule('visibility', 'hidden');
+    expect(wrapper.find('[data-id="child-content"]')).not.toExist();
   });
 
-  it('renders as visible when the "open" prop is true', () => {
+  it('renders content visible when the "open" prop is true', () => {
     const wrapper = subject({ open: true });
 
-    expect(wrapper.find('[role="dialog"]')).toHaveStyleRule('opacity', '1');
-    expect(wrapper.find('[role="dialog"]')).toHaveStyleRule('visibility', 'visible');
+    expect(wrapper.find('[data-id="child-content"]')).toExist();
   });
 
   it('renders with relevant ARIA attributes', () => {
