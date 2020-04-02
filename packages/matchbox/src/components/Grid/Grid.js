145987ab18059a2ab5ca6ece45734324c2bbd0d0
Brian Kemper <brian.kemper@sparkpost.com>
2020-04-02 14:37:22 -0400

    FE-948 Converted Grid to functional component

--- packages/matchbox/src/components/Grid/Grid.js
+++ packages/matchbox/src/components/Grid/Grid.js
@@ -1,51 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import classnames from 'classnames';
+import styled from 'styled-components';
+import { grid } from './styles';
 
 import Column from './Column';
-import styles from './Grid.module.scss';
 
-class Grid extends Component {
-  static displayName = 'Grid';
+const StyledGrid = styled('div')`
+  ${grid}
+`;
 
-  static Column = Column;
-
-  static propTypes = {
-    center: PropTypes.oneOf(['xs', 'sm', 'md', 'lg', 'xl']),
-    start: PropTypes.oneOf(['xs', 'sm', 'md', 'lg', 'xl']),
-    end: PropTypes.oneOf(['xs', 'sm', 'md', 'lg', 'xl']),
-    top: PropTypes.oneOf(['xs', 'sm', 'md', 'lg', 'xl']),
-    middle: PropTypes.oneOf(['xs', 'sm', 'md', 'lg', 'xl']),
-    bottom: PropTypes.oneOf(['xs', 'sm', 'md', 'lg', 'xl']),
-    around: PropTypes.oneOf(['xs', 'sm', 'md', 'lg', 'xl']),
-    between: PropTypes.oneOf(['xs', 'sm', 'md', 'lg', 'xl']),
-    children: PropTypes.node
-  };
-
-  render() {
-    const {
-      children,
-      start, center, end,
-      top, middle, bottom,
-      around, between,
-      className
-    } = this.props;
-
-    const gridClasses = classnames(
-      styles.Grid,
-      start && styles[`start-${start}`],
-      center && styles[`center-${center}`],
-      end && styles[`end-${end}`],
-      top && styles[`top-${top}`],
-      middle && styles[`middle-${middle}`],
-      bottom && styles[`bottom-${bottom}`],
-      around && styles[`around-${around}`],
-      between && styles[`between-${between}`],
-      className
-    );
-
-    return <div className={gridClasses}>{children}</div>;
-  }
+function Grid({ children, ...props }) {
+  return <StyledGrid {...props}>{children}</StyledGrid>;
 }
+
+Grid.displayName = 'Grid';
+Grid.Column = Column;
+
+Grid.propTypes = {
+  center: PropTypes.oneOf(['xs', 'sm', 'md', 'lg', 'xl']),
+  start: PropTypes.oneOf(['xs', 'sm', 'md', 'lg', 'xl']),
+  end: PropTypes.oneOf(['xs', 'sm', 'md', 'lg', 'xl']),
+  top: PropTypes.oneOf(['xs', 'sm', 'md', 'lg', 'xl']),
+  middle: PropTypes.oneOf(['xs', 'sm', 'md', 'lg', 'xl']),
+  bottom: PropTypes.oneOf(['xs', 'sm', 'md', 'lg', 'xl']),
+  around: PropTypes.oneOf(['xs', 'sm', 'md', 'lg', 'xl']),
+  between: PropTypes.oneOf(['xs', 'sm', 'md', 'lg', 'xl']),
+  children: PropTypes.node,
+};
 
 export default Grid;
---
+++ packages/matchbox/src/components/Grid/styles.js
@@ -0,0 +1,21 @@
+// todo, add margin-left: -$gutter-width;
+export const grid = `
+  box-sizing: border-box;
+  display: flex;
+  flex: 0 1 auto;
+  flex-direction: row;
+  flex-wrap: wrap;
+`;
+
+// todo, add to grid
+// const gridClasses = classnames(
+//   start && styles[`start-${start}`],
+//   center && styles[`center-${center}`],
+//   end && styles[`end-${end}`],
+//   top && styles[`top-${top}`],
+//   middle && styles[`middle-${middle}`],
+//   bottom && styles[`bottom-${bottom}`],
+//   around && styles[`around-${around}`],
+//   between && styles[`between-${between}`],
+//   className,
+// );
