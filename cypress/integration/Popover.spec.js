e482b87de787f01b2dd35fc581fed293101344e2
Jon Ambas <jon@jonambas.com>
2020-04-01 21:11:45 -0400

    FE-826 Remove flakey beforeEach, Comment out flakey slider test

--- cypress/integration/Popover.spec.js
+++ cypress/integration/Popover.spec.js
@@ -45,38 +45,35 @@ describe('Uncontrolled Popover with Actionlist', () => {
   });
 
   it('should open when clicking the trigger', () => {
-    cy.get('button')
-      .first()
-      .click();
+    cy.contains('More Actions').click();
     cy.get('[data-id="popover-content"]').should('be.visible');
   });
 
-  describe('when opened', () => {
-    beforeEach(() => {
-      cy.get('button')
-        .first()
-        .click();
-      cy.wait(200);
-    });
+  it('should toggle open with enter key', () => {
+    cy.contains('More Actions').type('{enter}');
+    cy.get('[data-id="popover-content"]').should('be.visible');
+  });
 
-    it('should tab through actionlist buttons', () => {
-      cy.get('[data-id="popover-content"]').should('be.visible');
-      cy.focused().tab();
-      cy.focused().should('have.text', 'Edit');
-      cy.focused().tab();
-      cy.focused().should('have.text', 'Delete');
-    });
+  it('should tab through actionlist buttons', () => {
+    cy.contains('More Actions').click();
+    cy.get('[data-id="popover-content"]').should('be.visible');
+    cy.focused().tab();
+    cy.focused().should('have.text', 'Edit');
+    cy.focused().tab();
+    cy.focused().should('have.text', 'Delete');
+  });
 
-    it('should close when clicking outside the popover', () => {
-      cy.get('[data-id="popover-content"]').should('be.visible');
-      cy.get('body').click(100, 300);
-      cy.get('[data-id="popover-content"]').should('not.be.visible');
-    });
+  it('should close when clicking outside the popover', () => {
+    cy.contains('More Actions').click();
+    cy.get('[data-id="popover-content"]').should('be.visible');
+    cy.get('body').click(100, 300);
+    cy.get('[data-id="popover-content"]').should('not.be.visible');
+  });
 
-    it('should close when pressing the escape key', () => {
-      cy.get('[data-id="popover-content"]').should('be.visible');
-      cy.get('body').type('{esc}');
-      cy.get('[data-id="popover-content"]').should('not.be.visible');
-    });
+  it('should close when pressing the escape key', () => {
+    cy.contains('More Actions').click();
+    cy.get('[data-id="popover-content"]').should('be.visible');
+    cy.get('body').type('{esc}');
+    cy.get('[data-id="popover-content"]').should('not.be.visible');
   });
 });
