7c468543d076e14991ed204bb0d319abbcf13026
Nick Lemmon <nick.lemmon@sparkpost.com>
2020-04-07 10:30:13 -0400

    FE-828 - Update CodeBlock Component (#373)
    * FE-828 - Wrapping up styles
    
    * FE-828 - Add support for dark theme
    
    * FE-828 - Add supporting unit tests
    
    * FE-828 - Add additional test for HTML semantics
    
    * FE-828 - TO REVERT AFTER APPROVAL - Add support for third party syntax highlighting
    
    * FE-828 - Add supporting unit test
    
    * FE-828 - Moves dependency to devDependencies
    
    * FE-828 - Correctly identify language
    
    * FE-828 - Uninstall unused dependency
    
    * FE-828 - Incorporate review feedback

--- stories/utility/CodeBlock.stories.js
+++ stories/utility/CodeBlock.stories.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
+import { addDecorator } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
-import { action } from '@storybook/addon-actions';
 import StoryContainer from '../storyHelpers/StoryContainer';
+import { ThemeProvider } from '@sparkpost/matchbox/components/ThemeProvider';
 import { CodeBlock, Panel } from '@sparkpost/matchbox';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { docco } from 'react-syntax-highlighter/styles/hljs';
 
-storiesOf('Utility|Code Block', module)
-  .addDecorator((getStory) => (
-    <StoryContainer>{ getStory() }</StoryContainer>
-  ))
-  .add('with chevrons', withInfo({ propTablesExclude: [Panel] })(() => (
-    <Panel sectioned>
-    <CodeBlock code={`curl -X POST
+const codeSnippet = `curl -X POST
 https://api.sparkpost.com/api/v1/transmissions
 -H "Authorization: api-key-here"
 -H "Content-Type: application/json"
@@ -25,26 +21,43 @@ https://api.sparkpost.com/api/v1/transmissions
     "text": "Sword of Omens, give me sight BEYOND sight"
   },
   "recipients": [{ "address": "email address here" }]
-}'`}/>
-    </Panel>
-  )))
+}'`;
 
-  .add('with numbers and height', withInfo({ propTablesExclude: [Panel] })(() => (
-    <Panel sectioned>
-    <CodeBlock numbered height={250} code={`curl -X POST
-https://api.sparkpost.com/api/v1/transmissions
--H "Authorization: api-key-here"
--H "Content-Type: application/json"
--d '{
-  "options": {
-    "sandbox": true
-  },
-  "content": {
-    "from": "from@email.com",
-    "subject": "Thundercats are GO!!!",
-    "text": "Sword of Omens, give me sight BEYOND sight"
-  },
-  "recipients": [{ "address": "email address here" }]
-}'`}/>
-    </Panel>
-  )));
+addDecorator(storyFn => (
+  <ThemeProvider>
+    <StoryContainer bg="white">
+      <Panel sectioned>{storyFn()}</Panel>
+    </StoryContainer>
+  </ThemeProvider>
+));
+
+export default {
+  title: 'Utility|Code Block',
+};
+
+export const Chevrons = withInfo({ propTables: [CodeBlock] })(() => (
+  <CodeBlock code={codeSnippet} />
+));
+
+export const Numbers = withInfo({ propTables: [CodeBlock] })(() => (
+  <CodeBlock numbered code={codeSnippet} />
+));
+
+export const Height = withInfo({ propTables: [CodeBlock] })(() => (
+  <CodeBlock numbered height={150} code={codeSnippet} />
+));
+
+export const Dark = withInfo({ propTables: [CodeBlock] })(() => (
+  <CodeBlock numbered dark code={codeSnippet} />
+));
+
+export const SyntaxHighlightingDemo = withInfo({ propTables: [CodeBlock] })(() => (
+  <CodeBlock numbered code={codeSnippet}>
+    <SyntaxHighlighter
+      PreTag={React.Fragment}
+      CodeTag={React.Fragment}
+      language="bash"
+      style={docco}
+    />
+  </CodeBlock>
+));
