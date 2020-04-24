187ec6d26a968b330680aa0b03d3b67dd7561a6c
Logan Sparlin <logan.sparlin@gmail.com>
2020-04-24 11:55:57 -0500

    FE-1009 Create LiveCode Component (#387)
    * Create Live Code Component
    
    * Resize Component and React Live implementation
    
    * fix resizing bug
    
    * update to use drag icon
    
    * beautify code example and change minWidth of preview
    
    * padding bottom on description
    
    * remove beautify

---
+++ packages/matchbox-site/src/components/LiveCode/LiveCode.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import Content from './LiveCodeContent';
+import { Select, Box } from '@sparkpost/matchbox';
+
+function LiveCode(props) {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const { children } = props;
+
+  function updateIndex(event) {
+    const index = Number(event.target.value);
+    setActiveIndex(index);
+  }
+
+  function getOptionsFromChildren() {
+    return children.map((child, index) => {
+      return {
+        value: index,
+        label: child.props.title
+      };
+    });
+  }
+
+  return (
+    <div>
+      <Box display="flex" alignItems="center">
+        <Box as="h6" m="0">
+          Examples
+        </Box>
+        <Box pl={500} flex="1">
+          <Select
+            id="code-example-select"
+            onChange={updateIndex}
+            options={getOptionsFromChildren()}
+          />
+        </Box>
+      </Box>
+      {children[activeIndex]}
+    </div>
+  );
+}
+LiveCode.displayName = 'LiveCode';
+LiveCode.Content = Content;
+
+export default LiveCode;
