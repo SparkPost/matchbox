0f1a47e0a991641f490adee5c8b7fd7faa345af9
Logan Sparlin <logan.sparlin@gmail.com>
2020-05-04 12:38:33 -0500

    FE-1026 Update Header Component (#397)
    * Convert Header to styled components
    
    * FE-1026

--- packages/matchbox-site/src/components/Header/Header.js
+++ packages/matchbox-site/src/components/Header/Header.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import MainNavigation from '../MainNavigation/MainNavigation';
 import { Link } from 'gatsby';
-import styles from './Header.module.scss';
+import styled from 'styled-components';
+
+import { Box } from '@sparkpost/matchbox';
+
+const StyledTitle = styled(Link)`
+  flex: 0 0 0;
+  text-decoration: none;
+`;
 
 function Header(props) {
   return (
-    <header className={styles.Header}>
-      <Link to="/" className={styles.Title}>
-        <h1 className={styles.TitleText}>Matchbox</h1>
-      </Link>
-      <div className={styles.NavWrapper}>
+    <Box
+      as="header"
+      display="flex"
+      alignItems="baseline"
+      mb="700"
+      pt="800"
+      pb="800"
+    >
+      <StyledTitle to="/">
+        <Box as="h1" fontSize="500" fontWeight="regular" color="gray.900">
+          Matchbox
+        </Box>
+      </StyledTitle>
+      <Box flex="1 0 0">
         <MainNavigation navItems={props.navItems} location={props.location} />
-      </div>
-    </header>
+      </Box>
+    </Box>
   );
 }
 
