0916ff00c58817a344575cc7d3cbb7307a1ee8f5
Jon Ambas <jon@jonambas.com>
2020-05-25 11:58:02 -0400

    Add more unit tests

--- packages/matchbox/src/components/Skeleton/tests/Skeleton.test.js
+++ packages/matchbox/src/components/Skeleton/tests/Skeleton.test.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import 'jest-styled-components';
-
+import cases from 'jest-in-case';
 import Skeleton, { Animator } from '../Skeleton';
 
 describe('Skeleton', () => {
   const Header = props => global.mountStyled(<Skeleton.Header {...props} />);
-  const Box = props => global.mountStyled(<Skeleton.Box {...props} />);
   const Body = props => global.mountStyled(<Skeleton.Body {...props} />);
+  const Box = props => global.mountStyled(<Skeleton.Box {...props} />);
+
+  const sizes = [
+    ['h1', '3.5rem'],
+    ['h2', '2.5rem'],
+    ['h3', '2rem'],
+    ['h4', '1.750rem'],
+    ['h5', '1.5rem'],
+    ['h6', '1.25rem'],
+  ];
 
   describe('Header', () => {
     it('should render with defaults', () => {
@@ -20,9 +29,18 @@ describe('Skeleton', () => {
       expect(wrapper.find('div').at(1)).toHaveStyleRule('max-width', '500px');
       expect(wrapper.find('div').at(1)).toHaveStyleRule('height', '3.5rem');
     });
+
+    cases(
+      'should look like headers',
+      ([looksLike, size]) => {
+        const wrapper = Header({ looksLike });
+        expect(wrapper.find('div').at(1)).toHaveStyleRule('height', size);
+      },
+      sizes,
+    );
   });
 
-  describe('Box', () => {
+  describe('Base Skeleton', () => {
     it('should render with box system props', () => {
       const wrapper = Box({ size: '20px' });
       expect(wrapper.find('div').at(1)).toHaveStyleRule('width', '20px');
