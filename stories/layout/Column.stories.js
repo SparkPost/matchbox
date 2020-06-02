40018c95401b7f590091255ea99a9a1dc8a244f8
Logan Sparlin <logan.sparlin@gmail.com>
2020-06-02 09:37:47 -0500

    Stories

--- stories/layout/Column.stories.js
+++ stories/layout/Column.stories.js
@@ -3,13 +3,48 @@ import { withInfo } from '@storybook/addon-info';
 import { Columns } from '@sparkpost/matchbox/components/Columns';
 import { Column } from '@sparkpost/matchbox/components/Column';
 
+import { DemoBlock } from './Columns.stories';
+
 export default {
   title: 'Layout|Column',
 };
 
 export const Default = withInfo()(() => (
-  <Columns>
-    <Column>Column 1</Column>
-    <Column>Column 2</Column>
-  </Columns>
+  <>
+    <Columns space="300">
+      <Column>
+        <DemoBlock>Fluid</DemoBlock>
+      </Column>
+      <Column width="content">
+        <DemoBlock>Content</DemoBlock>
+      </Column>
+    </Columns>
+    <Columns space="300">
+      <Column width={1 / 3}>
+        <DemoBlock>1/3</DemoBlock>
+      </Column>
+      <Column>
+        <DemoBlock>Fluid</DemoBlock>
+      </Column>
+    </Columns>
+    <Columns space="300">
+      <Column width={2 / 3}>
+        <DemoBlock>2/3</DemoBlock>
+      </Column>
+      <Column>
+        <DemoBlock>Fluid</DemoBlock>
+      </Column>
+    </Columns>
+    <Columns space="300">
+      <Column width={1 / 6}>
+        <DemoBlock>1/6</DemoBlock>
+      </Column>
+      <Column width={2 / 6}>
+        <DemoBlock>2/6</DemoBlock>
+      </Column>
+      <Column width={1 / 2}>
+        <DemoBlock>1/2</DemoBlock>
+      </Column>
+    </Columns>
+  </>
 ));
--- stories/layout/Columns.stories.js
+++ stories/layout/Columns.stories.js
@@ -8,37 +8,244 @@ export default {
   title: 'Layout|Columns',
 };
 
+export const DemoBlock = ({ height = 'auto', children, p = '300' }) => {
+  return (
+    <Box display="flex" alignItems="center" bg="blue.400" height={height} mb="18px" p={p}>
+      {children}
+    </Box>
+  );
+};
+
 export const Default = withInfo()(() => (
   <>
-    <Columns space="800">
-      <Column>Fluid</Column>
-      <Column width="content">content</Column>
+    <Columns space="300">
+      <Column>
+        <DemoBlock>Fluid</DemoBlock>
+      </Column>
+      <Column width="content">
+        <DemoBlock>Content</DemoBlock>
+      </Column>
+    </Columns>
+    <Columns space="300">
+      <Column width={[2 / 3, null, 1 / 3]}>
+        <DemoBlock>1/3</DemoBlock>
+      </Column>
+      <Column width={[1 / 3, null, 2 / 3]}>
+        <DemoBlock>2/3</DemoBlock>
+      </Column>
+    </Columns>
+    <Columns collapseBelow={'md'} space="300" reverse={[true, null, false]}>
+      <Column width={1 / 6}>
+        <DemoBlock>1/6</DemoBlock>
+      </Column>
+      <Column width={2 / 6}>
+        <DemoBlock>2/6</DemoBlock>
+      </Column>
+      <Column width={1 / 2}>
+        <DemoBlock>1/2</DemoBlock>
+      </Column>
+    </Columns>
+  </>
+));
+
+export const Space = withInfo()(() => (
+  <>
+    <Columns space="100">
+      <Column>
+        <DemoBlock>100</DemoBlock>
+      </Column>
+      <Column>
+        <DemoBlock>100</DemoBlock>
+      </Column>
+      <Column>
+        <DemoBlock>100</DemoBlock>
+      </Column>
+    </Columns>
+    <Columns space="200">
+      <Column>
+        <DemoBlock>200</DemoBlock>
+      </Column>
+      <Column>
+        <DemoBlock>200</DemoBlock>
+      </Column>
+      <Column>
+        <DemoBlock>200</DemoBlock>
+      </Column>
+    </Columns>
+    <Columns space="300">
+      <Column>
+        <DemoBlock>300</DemoBlock>
+      </Column>
+      <Column>
+        <DemoBlock>300</DemoBlock>
+      </Column>
+      <Column>
+        <DemoBlock>300</DemoBlock>
+      </Column>
     </Columns>
     <Columns space="400">
-      <Column width={[2 / 3, null, 1 / 3]}>1/3</Column>
-      <Column width={[1 / 3, null, 2 / 3]}>2/3</Column>
-    </Columns>
-    <Columns space="800" alignY={['bottom', null, 'center']}>
       <Column>
-        <Box height="800" bg="blue.700">
-          <span>Column 1</span>
-        </Box>
+        <DemoBlock>400</DemoBlock>
       </Column>
       <Column>
-        <Box bg="blue.700">Column 2</Box>
+        <DemoBlock>400</DemoBlock>
+      </Column>
+      <Column>
+        <DemoBlock>400</DemoBlock>
       </Column>
     </Columns>
-    <Columns mt="400" space="300" align={['left', null, 'right']}>
+    <Columns space="500">
+      <Column>
+        <DemoBlock>500</DemoBlock>
+      </Column>
+      <Column>
+        <DemoBlock>500</DemoBlock>
+      </Column>
+      <Column>
+        <DemoBlock>500</DemoBlock>
+      </Column>
+    </Columns>
+    <Columns space="600">
+      <Column>
+        <DemoBlock>600</DemoBlock>
+      </Column>
+      <Column>
+        <DemoBlock>600</DemoBlock>
+      </Column>
+      <Column>
+        <DemoBlock>600</DemoBlock>
+      </Column>
+    </Columns>
+    <Columns space="700">
+      <Column>
+        <DemoBlock>700</DemoBlock>
+      </Column>
+      <Column>
+        <DemoBlock>700</DemoBlock>
+      </Column>
+      <Column>
+        <DemoBlock>700</DemoBlock>
+      </Column>
+    </Columns>
+    <Columns space="800">
+      <Column>
+        <DemoBlock>800</DemoBlock>
+      </Column>
+      <Column>
+        <DemoBlock>800</DemoBlock>
+      </Column>
+      <Column>
+        <DemoBlock>800</DemoBlock>
+      </Column>
+    </Columns>
+  </>
+));
+
+export const Alignment = withInfo()(() => (
+  <>
+    <Columns space="300" align={['right', null, 'left']}>
       <Column width={1 / 2}>
-        <Box height="800" bg="blue.700">
-          <span>Column 1</span>
-        </Box>
+        <DemoBlock>Left Aligned</DemoBlock>
       </Column>
     </Columns>
-    <Columns collapseBelow={'md'} mt="400" space="300" reverse={[true, null, false]}>
-      <Column width={1 / 3}>Column 1</Column>
-      <Column width={1 / 3}>Column 2</Column>
-      <Column width={1 / 3}>Column 3</Column>
+    <Columns space="300" align="center">
+      <Column width={1 / 2}>
+        <DemoBlock>Center Aligned</DemoBlock>
+      </Column>
+    </Columns>
+    <Columns space="300" align={['left', null, 'right']}>
+      <Column width={1 / 2}>
+        <DemoBlock>Right Aligned</DemoBlock>
+      </Column>
+    </Columns>
+    <Columns mt={600} space="800" alignY={['bottom', null, 'center']}>
+      <Column>
+        <DemoBlock height={800} p={700}>
+          Column 1
+        </DemoBlock>
+      </Column>
+      <Column>
+        <DemoBlock>Column 2</DemoBlock>
+      </Column>
+    </Columns>
+  </>
+));
+
+export const Reverse = withInfo()(() => (
+  <>
+    <Columns space="300" reverse>
+      <Column>
+        <DemoBlock>Column 1</DemoBlock>
+      </Column>
+      <Column>
+        <DemoBlock>Column 2</DemoBlock>
+      </Column>
+      <Column>
+        <DemoBlock>Column 3</DemoBlock>
+      </Column>
+    </Columns>
+    <Columns space="300" reverse={[true, null, false]}>
+      <Column>
+        <DemoBlock>Column 1</DemoBlock>
+      </Column>
+      <Column>
+        <DemoBlock>Column 2</DemoBlock>
+      </Column>
+      <Column>
+        <DemoBlock>Column 3</DemoBlock>
+      </Column>
+    </Columns>
+  </>
+));
+
+export const CollapseBelow = withInfo()(() => (
+  <Columns space="300" collapseBelow="md">
+    <Column>
+      <DemoBlock>Column 1</DemoBlock>
+    </Column>
+    <Column>
+      <DemoBlock>Column 2</DemoBlock>
+    </Column>
+    <Column>
+      <DemoBlock>Column 3</DemoBlock>
+    </Column>
+  </Columns>
+));
+
+export const SystemProps = withInfo()(() => (
+  <>
+    <Columns space="300" mb={600}>
+      <Column>
+        <DemoBlock>Column 1</DemoBlock>
+      </Column>
+      <Column>
+        <DemoBlock>Column 2</DemoBlock>
+      </Column>
+      <Column>
+        <DemoBlock>Column 3</DemoBlock>
+      </Column>
+    </Columns>
+    <Columns space="300" mb={200}>
+      <Column>
+        <DemoBlock>Column 1</DemoBlock>
+      </Column>
+      <Column>
+        <DemoBlock>Column 2</DemoBlock>
+      </Column>
+      <Column>
+        <DemoBlock>Column 3</DemoBlock>
+      </Column>
+    </Columns>
+    <Columns space="300" ml={800} mr={800}>
+      <Column>
+        <DemoBlock>Column 1</DemoBlock>
+      </Column>
+      <Column>
+        <DemoBlock>Column 2</DemoBlock>
+      </Column>
+      <Column>
+        <DemoBlock>Column 3</DemoBlock>
+      </Column>
     </Columns>
   </>
 ));
