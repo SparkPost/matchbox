780bb1f8067bf7eb6542e9d7bc3612f7edeaf3e9
Nick Lemmon <nick.lemmon@sparkpost.com>
2020-06-09 09:56:09 -0400

    Resolve issue #194 by adding relevant aria attributes to the ProgressBar component (#245)
    * Issue 194 Incorporate role progressbar and relevant aria attributes
    
    * Issue 194 Updated tests to leverage new props
    
    * Update tests for a11y behavior
    
    * Update unreleased.md
    
    Co-authored-by: Jon Ambas <jon@jonambas.com>

--- packages/matchbox/src/components/ProgressBar/tests/ProgressBar.test.js
+++ packages/matchbox/src/components/ProgressBar/tests/ProgressBar.test.js
@@ -51,21 +51,52 @@ cases(
 describe('renders progress bar inner styles', () => {
   const progressBar = props => global.mountStyled(<ProgressBar {...props} />);
 
+  it('Has relevant WAI-ARIA attributes', () => {
+    const wrapper = progressBar({ label: 'this is a test progress bar', completed: 10 });
+    expect(wrapper.find('[role="progressbar"]')).toExist();
+    expect(wrapper.find('[aria-valuenow]')).toExist();
+    expect(wrapper.find('[aria-valuemin]')).toExist();
+    expect(wrapper.find('[aria-valuemax]')).toExist();
+  });
+
   it('renders progress bar width based on completed percent', () => {
     const wrapper = progressBar({ completed: 24 });
-
     expect(wrapper.find('div').at(1)).toHaveStyleRule('width', '24%');
+    expect(
+      wrapper
+        .find('div')
+        .at(0)
+        .prop('aria-valuenow'),
+    ).toEqual('24');
   });
 
   it('renders at 100% progress if given a value over 100', () => {
     const wrapper = progressBar({ completed: 110 });
-
     expect(wrapper.find('div').at(1)).toHaveStyleRule('width', '100%');
   });
 
   it('renders at 0% progress if given a negative value', () => {
     const wrapper = progressBar({ completed: -10 });
-
     expect(wrapper.find('div').at(1)).toHaveStyleRule('width', '0%');
+  });
+
+  it('renders at custom value text', () => {
+    const wrapper = progressBar({ valueText: 'test', completed: 10 });
+    expect(
+      wrapper
+        .find('div')
+        .first()
+        .prop('aria-valuetext'),
+    ).toEqual('test');
+  });
+
+  it('renders a screen reader label', () => {
+    const wrapper = progressBar({ label: 'test', completed: 10 });
+    expect(
+      wrapper
+        .find('div')
+        .first()
+        .prop('aria-label'),
+    ).toEqual('test');
   });
 });
