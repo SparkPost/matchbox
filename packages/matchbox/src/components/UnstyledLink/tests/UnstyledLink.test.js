ecb5452977f965f087017112853fc72e1b0cf770
Jon Ambas <jon@jonambas.com>
2020-06-09 10:48:44 -0400

    Add role=button to UnstylLinks with an onClick

--- packages/matchbox/src/components/UnstyledLink/tests/UnstyledLink.test.js
+++ packages/matchbox/src/components/UnstyledLink/tests/UnstyledLink.test.js
@@ -8,20 +8,34 @@ describe('UnstyledLink', () => {
   it('renders an external link', () => {
     let wrapper = subject({ to: '/external', external: true });
     expect(wrapper).toHaveAttributeValue('target', '_blank');
+    expect(wrapper.prop('role')).toBeUndefined();
+  });
+
+  it('renders with a role', () => {
+    let wrapper = subject({ role: 'button' });
+    expect(wrapper.find('a').prop('role')).toEqual('button');
+  });
+
+  it('renders with a role with onClick', () => {
+    let wrapper = subject({ onClick: jest.fn() });
+    expect(wrapper.find('a').prop('role')).toEqual('button');
   });
 
   it('renders with title', () => {
     let wrapper = subject({ title: 'The Title' });
     expect(wrapper).toHaveAttributeValue('title', 'The Title');
+    expect(wrapper.prop('role')).toBeUndefined();
   });
 
   it('renders href', () => {
     let wrapper = subject({ to: '/link' });
     expect(wrapper).toHaveAttributeValue('href', '/link');
+    expect(wrapper.prop('role')).toBeUndefined();
   });
 
   it('renders with wrapper component', () => {
     let wrapper = subject({ component: 'button' });
     expect(wrapper.find('button').text()).toEqual('Hola!');
+    expect(wrapper.prop('role')).toBeUndefined();
   });
 });
