ba8956261e4e90780081eb837df91707aed58c23
Jon Ambas <jon@jonambas.com>
2020-06-12 16:11:16 -0400

    Add Snackbar page, Add prettier to react-live (#459)
    * Add live code formatting, Snackbar page
    
    * Style live error

--- packages/matchbox-site/src/components/LiveCode/LiveCodeContent.js
+++ packages/matchbox-site/src/components/LiveCode/LiveCodeContent.js
@@ -7,25 +7,48 @@ import ResizeContainer from '../ResizeContainer/ResizeContainer';
 import { Box } from '@sparkpost/matchbox';
 import * as components from '@sparkpost/matchbox';
 
+import prettier from 'prettier/standalone';
+import parserBabel from 'prettier/parser-babel';
+
 const StyledEditor = styled(LiveEditor)`
   background: ${tokens.color_blue_1000};
   font-size: ${tokens.fontSize_100};
+  ${'' /* // controls the blinking cursor color */}
+  color: ${tokens.color_blue_200};
+  * {
+    outline: none;
+  }
+`;
+
+const StyledError = styled(LiveError)`
+  background: ${tokens.color_red_800};
+  font-size: ${tokens.fontSize_100};
+  color: ${tokens.color_red_100};
 `;
 
 function Content(props) {
   const { description, disableResize, code } = props;
+
+  const formatted = prettier.format(code.trim(), {
+    parser: 'babel',
+    plugins: [parserBabel],
+    tabWidth: 2,
+    jsxSingleQuote: false
+  });
 
   return (
     <Box mt="600" mb="600">
       <Box as="p" mb="500" pb="0">
         {description}
       </Box>
-      <LiveProvider code={code} scope={components}>
+      <LiveProvider code={formatted.trim()} scope={components}>
         <ResizeContainer disableResize={disableResize}>
           <LivePreview />
         </ResizeContainer>
-        <StyledEditor />
-        <LiveError />
+        <Box bg="blue.1000" p="400" fontSize="100">
+          <StyledEditor />
+        </Box>
+        <StyledError />
       </LiveProvider>
     </Box>
   );
