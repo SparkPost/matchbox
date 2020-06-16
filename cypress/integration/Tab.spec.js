742a0a6b3f73d4119ab2e1ae4d2a7ce86054a51f
Jon Ambas <jon@jonambas.com>
2020-06-15 21:40:20 -0400

    UX-155 Add Tab keyboard navigation (#413)
    * UX-155 Add keyboard navigation for tabs
    
    * UX-155 Update tab cypress tests
    
    * UX-155 Update tab unit test
    
    * UX-155 Add a reusable useTabs hook, document the hook and component
    
    * Update unreleased.md
    
    * UX-155 Reset tabindex on container blur
    
    * Fix a bad rebase
    
    * Adjust tab cypress test
    
    * Adjust codecov patch target
    
    * Add ref pass through
    
    * Fix bad rebase
    
    * Fix bad rebasex2
    
    * Update Text stories

--- cypress/integration/Tab.spec.js
+++ cypress/integration/Tab.spec.js
@@ -13,26 +13,68 @@ describe('The Tabs component', () => {
         .eq(1)
         .click();
       cy.get('[aria-selected="true"]').should('have.text', 'More Details');
+      cy.get(`[tabindex="0"]`).should('have.text', 'Details');
+      cy.get(`[tabindex="-1"]`).should('have.length', 4);
     });
+  });
 
-    it('should handle keyboard navigation', () => {
+  describe('keyboard navigation', () => {
+    beforeEach(() => {
+      cy.visit('/iframe.html?path=/story/navigation-tabs--example-tabs');
+      // Setting viewport dimensions to avoid side effects
+      cy.viewport(1400, 600);
       cy.get('button')
         .first()
         .click();
-      cy.focused().tab();
+    });
+
+    it('should handle arrow keys', () => {
+      cy.get('body').type('{rightArrow}');
       cy.focused().should('have.text', 'More Details');
-      cy.focused().tab();
-      cy.focused().should('have.text', 'Example with long text');
+      cy.get('body').type('{rightArrow}');
+      cy.get('body').type('{rightArrow}');
+      cy.get('body').type('{rightArrow}');
+      cy.focused().should('have.text', 'Details');
+      cy.get('body').type('{leftArrow}');
+      cy.focused().should('have.text', 'Example with a component wrapper');
+      cy.get(`[tabindex="0"]`).should('have.text', 'Example with a component wrapper');
+    });
+
+    it('should handle home and end keys', () => {
+      cy.get('body').type('{end}');
+      cy.focused().should('have.text', 'Example with a component wrapper');
+      cy.get('body').type('{home}');
+      cy.focused().should('have.text', 'Details');
+    });
+
+    it('should handle page up and page down keys', () => {
+      cy.get('body').type('{pageUp}');
+      cy.focused().should('have.text', 'Example with a component wrapper');
+      cy.get('body').type('{pageDown}');
+      cy.focused().should('have.text', 'Details');
+    });
+
+    it('should tab past the tabs', () => {
+      cy.tab();
+      cy.focused().should('have.text', 'this is only here to test focus order');
+    });
+
+    it('should correctly reset tabindex to selected tab after blurring', () => {
+      cy.get('body').type('{rightArrow}');
+      cy.focused().should('have.text', 'More Details');
+      cy.tab();
+      cy.focused().should('have.text', 'this is only here to test focus order');
+      cy.get(`[tabindex="0"]`).should('have.text', 'Details');
     });
   });
 
   // Tests are flakey because they require layout changes
   // Remove if these become a problem
-  describe.skip('when overflowing', () => {
+  describe('when overflowing', () => {
     beforeEach(() => {
       cy.visit('/iframe.html?path=/story/navigation-tabs--example-tabs');
       cy.viewport(800, 600);
-      cy.wait(400);
+      cy.wait(500);
     });
 
     it('should handle actionlist click', () => {
@@ -53,11 +95,9 @@ describe('The Tabs component', () => {
       cy.get('[data-id="popover-content"]').should('not.be.visible');
     });
 
-    it('should handle keyboard navigation', () => {
+    it('should focus on the menu', () => {
       cy.get('[data-id="tab-options-button"]').click();
-      cy.focused().should('have.text', 'More Options');
-      cy.focused().tab();
-      cy.focused().should('have.text', 'More Details');
+      cy.get('[data-id="popover-focus-wrapper"]').should('have.focus');
     });
   });
 });
