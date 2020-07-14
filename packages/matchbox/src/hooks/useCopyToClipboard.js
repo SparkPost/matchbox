38ef7524c759f20eab41c40d5388935d10e0679f
Jon Ambas <jon@jonambas.com>
2020-07-14 11:37:03 -0400

    UX-247 Add useCopyToClipboard hook (#533)
    * Add useCopyToClipboard hook
    
    * Add useCopyToClipboard hook story and copy handler
    
    * Add useCopyToClipboard test
    
    * Update story title

---
+++ packages/matchbox/src/hooks/useCopyToClipboard.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { getWindow } from '../helpers/window';
+import copy from 'copy-to-clipboard';
+
+/**
+ * SSR friendly hook that copies a string to the clipboard and provides copied state
+ *
+ * @example
+ * const { copy, copied } = useCopyToClipboard({ timeout: 2000 });
+ *
+ * <Button onClick={() => copy('string')}>
+ *  {copied ? 'String has been copied' : 'Copy to clipboard'}
+ * </Button>
+ */
+function useCopyToClipboard({ timeout = 1000 } = {}) {
+  const environment = getWindow();
+  const [copied, setCopied] = React.useState(false);
+
+  function handleCopy(string) {
+    copy(string);
+    setCopied(true);
+  }
+
+  React.useEffect(() => {
+    let timer;
+    if (copied) {
+      timer = environment.setTimeout(() => setCopied(false), timeout);
+    }
+    return () => {
+      environment.clearTimeout(timer);
+    };
+  }, [copied, timeout]);
+
+  return {
+    copy: handleCopy,
+    copied,
+  };
+}
+
+export default useCopyToClipboard;
