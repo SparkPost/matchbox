caad3997417dae0768df0282d73586710d4dc1b1
Logan Sparlin <logan.sparlin@gmail.com>
2020-07-24 11:47:00 -0500

    Pass tests

--- packages/matchbox/src/components/Layout/tests/Section.test.js
+++ packages/matchbox/src/components/Layout/tests/Section.test.js
@@ -24,14 +24,34 @@ describe('Layout', () => {
   it('renders two column section', () => {
     const wrapper = subject();
 
-    expect(wrapper.find('div').at(2)).toHaveStyleRule('width', '50%');
-    expect(wrapper.find('div').at(3)).toHaveStyleRule('width', '50%');
+    expect(
+      wrapper
+        .find('div')
+        .at(2)
+        .text(),
+    ).toEqual('Two Column Layout');
+    expect(
+      wrapper
+        .find('div')
+        .at(3)
+        .text(),
+    ).toEqual('Two Column Layout');
   });
 
   it('renders annotated section', () => {
     const wrapper = subject();
 
-    expect(wrapper.find('div').at(4)).toHaveStyleRule('width', '300px');
-    expect(wrapper.find('div').at(5)).toHaveStyleRule('width', 'calc(100% - 300px)');
+    expect(
+      wrapper
+        .find('div')
+        .at(4)
+        .text(),
+    ).toEqual('Annotated Layout');
+    expect(
+      wrapper
+        .find('div')
+        .at(5)
+        .text(),
+    ).toEqual('Annotated Layout');
   });
 });
