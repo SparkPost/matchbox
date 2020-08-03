13787b9b834ff837f75867ae95e5979bac4f4f6f
Logan Sparlin <logan.sparlin@gmail.com>
2020-08-03 13:16:38 -0500

    update lerna.json and webpack, rename videos and add in last frame screenshots

--- stories/media/video.stories.js
+++ stories/media/video.stories.js
@@ -4,32 +4,42 @@ import { withInfo } from '@storybook/addon-info';
 
 import { Box, Text } from '@sparkpost/matchbox';
 
-import Video from '@sparkpost/matchbox-media/videos/analytics_169_webm.webm';
+import Video from '@sparkpost/matchbox-media/videos/Analytics.webm';
+import Image from '@sparkpost/matchbox-media/videos/Analytics-LastFrame.png';
 
 const VIDEOS_WEBM = {
-  'analytics_169_webm.webm': require('@sparkpost/matchbox-media/videos/analytics_169_webm.webm'),
-  'analytics_crop_webm.webm': require('@sparkpost/matchbox-media/videos/analytics_crop_webm.webm'),
-  'recipient_validation_169_webm.webm': require('@sparkpost/matchbox-media/videos/recipient_validation_169_webm.webm'),
-  'recipient_validation_crop_webm.webm': require('@sparkpost/matchbox-media/videos/recipient_validation_crop_webm.webm'),
-  'signup_169_webm.webm': require('@sparkpost/matchbox-media/videos/signup_169_webm.webm'),
+  'Analytics.webm': require('@sparkpost/matchbox-media/videos/Analytics.webm'),
+  'Analytics-Crop.webm': require('@sparkpost/matchbox-media/videos/Analytics-Crop.webm'),
+  'Recipient-Validation.webm': require('@sparkpost/matchbox-media/videos/Recipient-Validation.webm'),
+  'Recipient-Validation-Crop.webm': require('@sparkpost/matchbox-media/videos/Recipient-Validation-Crop.webm'),
+  'Signup.webm': require('@sparkpost/matchbox-media/videos/Signup.webm'),
+  'Signup-Crop.webm': require('@sparkpost/matchbox-media/videos/Signup-Crop.webm'),
 };
 
 const VIDEOS_MP4 = {
-  'analytics_169_h264.mp4': require('@sparkpost/matchbox-media/videos/analytics_169_h264.mp4'),
-  'analytics_crop_h264.mp4': require('@sparkpost/matchbox-media/videos/analytics_crop_h264.mp4'),
-  'recipient_validation_169_h264.mp4': require('@sparkpost/matchbox-media/videos/recipient_validation_169_h264.mp4'),
-  'recipient_validation_crop_h264.mp4': require('@sparkpost/matchbox-media/videos/recipient_validation_crop_h264.mp4'),
-  'signup_169_h264.mp4': require('@sparkpost/matchbox-media/videos/signup_169_h264.mp4'),
-  'signup_crop_h264.mp4': require('@sparkpost/matchbox-media/videos/signup_crop_h264.mp4'),
+  'Analytics.mp4': require('@sparkpost/matchbox-media/videos/Analytics.mp4'),
+  'Analytics-Crop.mp4': require('@sparkpost/matchbox-media/videos/Analytics-Crop.mp4'),
+  'Recipient-Validation.mp4': require('@sparkpost/matchbox-media/videos/Recipient-Validation.mp4'),
+  'Recipient-Validation-Crop.mp4': require('@sparkpost/matchbox-media/videos/Recipient-Validation-Crop.mp4'),
+  'Signup.mp4': require('@sparkpost/matchbox-media/videos/Signup.mp4'),
+  'Signup-Crop.mp4': require('@sparkpost/matchbox-media/videos/Signup-Crop.mp4'),
 };
 
 storiesOf('Media|Video', module)
   .add(
     'single video',
     withInfo()(() => (
-      <video width="800px" autoPlay muted>
-        <source src={Video} type="video/webm" />
-      </video>
+      <Box display="flex">
+        <Box flex="1">
+          <video width="100%" autoPlay muted>
+            <source src={Video} type="video/webm" />
+          </video>
+        </Box>
+        <Box flex="1" textAlign="center">
+          <Box as="img" width="100%" src={Image} />
+          <Text>Last Frame</Text>
+        </Box>
+      </Box>
     )),
   )
 
