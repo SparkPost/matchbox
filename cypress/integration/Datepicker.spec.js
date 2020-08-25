ab39af4043db043a43fb120d726ca967d38e935b
Jon Ambas <jon@jonambas.com>
2020-08-25 19:05:28 -0400

    UX-288 Set viewport before visiting page

--- cypress/integration/Datepicker.spec.js
+++ cypress/integration/Datepicker.spec.js
@@ -1,17 +1,15 @@
 /// <reference types="Cypress" />
 
 describe('The Datepicker component', () => {
-  beforeEach(() => {
+  it('should render 1 months', () => {
+    cy.viewport(300, 660);
     cy.visit('/iframe.html?path=/story/form-datepicker--basic-datepicker');
+    cy.get('.DayPicker-Month').should('have.length', 1);
   });
 
   it('should render 2 months', () => {
     cy.viewport(1000, 660);
+    cy.visit('/iframe.html?path=/story/form-datepicker--basic-datepicker');
     cy.get('.DayPicker-Month').should('have.length', 2);
-  });
-
-  it('should render 1 months', () => {
-    cy.viewport(400, 660);
-    cy.get('.DayPicker-Month').should('have.length', 1);
   });
 });
