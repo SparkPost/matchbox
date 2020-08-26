19134c1bef77930f33b87b9a334b067fe611f2d6
Jon Ambas <jon@jonambas.com>
2020-08-26 10:20:12 -0400

    UX-324 Update filterByVisible to fail gracefully

--- packages/matchbox/src/helpers/tests/array.test.js
+++ packages/matchbox/src/helpers/tests/array.test.js
@@ -1,32 +1,30 @@
 import { groupBy, groupByValues, filterByVisible } from '../array';
 
 describe('Helper: Array helpers', () => {
-
   let list;
 
   beforeEach(() => {
     list = [
       {
         name: 'a',
-        section: 1
+        section: 1,
       },
       {
         name: 'b',
-        section: 4
+        section: 4,
       },
       {
         name: 'c',
-        section: 1
+        section: 1,
       },
       {
         name: 'd',
-        section: 'mixed whoa'
-      }
+        section: 'mixed whoa',
+      },
     ];
   });
 
   describe('groupBy', () => {
-
     it('should group a list by a given key', () => {
       expect(groupBy(list, 'section')).toMatchSnapshot();
     });
@@ -44,25 +42,29 @@ describe('Helper: Array helpers', () => {
       expect(Object.keys(grouped)).toHaveLength(1);
       expect(grouped).toMatchSnapshot();
     });
-
   });
 
   describe('groupByValues', () => {
-
     it('should return a grouped array', () => {
       expect(groupByValues(list, 'section')).toMatchSnapshot();
     });
-
   });
 
   describe('filterByVisible', () => {
     it('should return only visible actions', () => {
-      expect(filterByVisible([
-        { content: '1' },
-        { content: '2', visible: true },
-        { content: '3', visible: false }
-      ])).toMatchSnapshot();
+      expect(
+        filterByVisible([
+          { content: '1' },
+          { content: '2', visible: true },
+          { content: '3', visible: false },
+        ]),
+      ).toMatchSnapshot();
+    });
+
+    it('should fail gracefully', () => {
+      expect(filterByVisible(null)).toEqual([]);
+      expect(filterByVisible(undefined)).toEqual([]);
+      expect(filterByVisible()).toEqual([]);
     });
   });
-
 });
