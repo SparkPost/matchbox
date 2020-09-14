fbd8798114d69bdb40bcbb5e34cf1bc39530dbb4
Logan Sparlin <logan.sparlin@gmail.com>
2020-09-14 09:15:45 -0500

    UX-325 Panel.SubHeader and tests

--- packages/matchbox/src/components/Panel/SubHeader.js
+++ packages/matchbox/src/components/Panel/SubHeader.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Box } from '../Box';
+import { PanelPaddingContext } from './context';
 
-function SubHeader() {
-  return <Box as="p">SubHeader</Box>;
-}
+const SubHeader = React.forwardRef(function SubHeader(props, userRef) {
+  const { children, className } = props;
+
+  const paddingContext = React.useContext(PanelPaddingContext);
+
+  return (
+    <Box
+      as="p"
+      {...paddingContext}
+      className={className}
+      pb={[0, null, 0]}
+      fontSize="200"
+      color="gray.700"
+      tabIndex="-1"
+      ref={userRef}
+    >
+      {children}
+    </Box>
+  );
+});
 
 SubHeader.displayName = 'Panel.SubHeader';
+
+SubHeader.propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string,
+};
 
 export default SubHeader;
