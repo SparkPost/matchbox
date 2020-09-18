2542bd413e50b63c52f73cbdc3ad160855a785c7
Logan Sparlin <logan.sparlin@gmail.com>
2020-09-18 16:06:47 -0500

    Update cypress tests

--- cypress/integration/ListBox.spec.js
+++ cypress/integration/ListBox.spec.js
@@ -3,13 +3,41 @@
 describe('The ListBox component', () => {
   beforeEach(() => {
     cy.visit(
-      '/iframe.html?selectedKind=Form%7CListBox&selectedStory=Basic%20Listbox&full=0&addons=1&stories=1&panelRight=0&addonPanel=storybook%2Factions%2Factions-panel',
+      '/iframe.html?selectedKind=Form%7CListBox&selectedStory=Printable%20Character&full=0&addons=1&stories=1&panelRight=0&addonPanel=storybook%2Factions%2Factions-panel',
     );
   });
 
   it('opens the listbox when clicking on the `<label>`', () => {
     cy.get('label').click();
 
-    cy.contains('Option 2').should('be.visible');
+    cy.contains('Alpha').should('be.visible');
+  });
+
+  it('searches options when typing', () => {
+    cy.get('label').click();
+
+    cy.get('[data-id="open-listbox"]').trigger('keydown', {
+      key: 'a',
+      keyCode: 65,
+      shiftKey: false,
+    });
+  });
+
+  it('selects focused option when clicking enter key', () => {
+    cy.get('label').click();
+
+    cy.get('[data-id="open-listbox"]').trigger('keydown', {
+      key: 'a',
+      keyCode: 65,
+      shiftKey: false,
+    });
+
+    cy.get('[data-id="open-listbox"]').trigger('keydown', {
+      key: 'Enter',
+      keycode: 13,
+      shiftKey: false,
+    });
+
+    cy.contains('Alpha').should('be.visible');
   });
 });
