aa972d2cdd16f1f3530f9e612e0cd07218c26647
Logan Sparlin <logan.sparlin@gmail.com>
2020-09-18 12:08:14 -0500

    UX-336 Padding system prop on Modal.Content (#641)

--- packages/matchbox/src/components/Modal/Content.js
+++ packages/matchbox/src/components/Modal/Content.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { padding } from 'styled-system';
+import { createPropTypes } from '@styled-system/prop-types';
+import { pick } from '../../helpers/systemProps';
 
 import { Box } from '../Box';
 
 const Content = React.forwardRef(function Content(props, ref) {
-  const { children } = props;
+  const { children, ...rest } = props;
+  const systemProps = pick(rest, padding.propNames);
 
   return (
-    <Box data-id="modal-content" p="500" maxHeight="60vh" overflowY="auto" ref={ref}>
+    <Box data-id="modal-content" {...systemProps} maxHeight="60vh" overflowY="auto" ref={ref}>
       {children}
     </Box>
   );
@@ -16,6 +20,11 @@ const Content = React.forwardRef(function Content(props, ref) {
 Content.displayName = 'Modal.Content';
 Content.propTypes = {
   children: PropTypes.node,
+  ...createPropTypes(padding.propNames),
+};
+
+Content.defaultProps = {
+  p: '500',
 };
 
 export default Content;
