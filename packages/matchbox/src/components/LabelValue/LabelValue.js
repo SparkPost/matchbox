a451e784fe78f5508110c51590946c50a41d0b63
Jon Ambas <jon@jonambas.com>
2020-09-21 11:20:27 -0400

    UX-331 Add LabelValue component

---
+++ packages/matchbox/src/components/LabelValue/LabelValue.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { createPropTypes } from '@styled-system/prop-types';
+import styled from 'styled-components';
+import { margin } from 'styled-system';
+import { pick } from '../../helpers/systemProps';
+import { Box } from '../Box';
+
+const StyledWrapper = styled.div`
+  ${margin}
+`;
+
+const LabelValue = React.forwardRef(function LabelValue(props, userRef) {
+  const { label, children, className, orientation, ...rest } = props;
+
+  const systemProps = pick(rest, margin.propNames);
+
+  return (
+    <StyledWrapper className={className} {...systemProps} ref={userRef}>
+      <Box
+        display={orientation === 'horizontal' ? 'grid' : 'block'}
+        gridGap={orientation === 'horizontal' ? '300' : ''}
+        gridTemplateColumns={orientation === 'horizontal' ? '11rem 1fr' : ''}
+      >
+        <Box fontSize="200" fontWeight="semibold" mb={orientation === 'vertical' ? '100' : ''}>
+          {label}
+        </Box>
+        <Box>{children}</Box>
+      </Box>
+    </StyledWrapper>
+  );
+});
+
+LabelValue.displayName = 'LabelValue';
+LabelValue.propTypes = {
+  label: PropTypes.string,
+  children: PropTypes.node,
+  className: PropTypes.string,
+  orientation: PropTypes.oneOf(['horizontal', 'vertical']),
+  ...createPropTypes(margin.propNames),
+};
+
+LabelValue.defaultProps = {
+  orientation: 'vertical',
+};
+
+export default LabelValue;
