c3e374f6db5d8244efa8f4c9b2fe06e6ef2b2b05
Logan Sparlin <losparlin@Logan-Sparlins-MacBook-Pro.local>
2020-10-09 11:59:24 -0500

    UX-341 EmptyState.List dotted line

--- packages/matchbox/src/components/EmptyState/List.js
+++ packages/matchbox/src/components/EmptyState/List.js
@@ -1,21 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { tokens } from '@sparkpost/design-tokens';
+import styled from 'styled-components';
 import { StyledList } from './styles';
+
+const StyledSvg = styled.svg`
+  position: absolute;
+  left: 3px;
+  top: 18px;
+`;
 
 const List = React.forwardRef(function List(props, userRef) {
   const { children } = props;
 
   return (
     <StyledList as="ul" mt="500" position="relative" ref={userRef}>
-      {React.Children.map(children, child => {
-        return React.cloneElement(child, { children: <div>{children} Hello</div> });
+      {React.Children.map(children, (child, index) => {
+        return React.cloneElement(child, {
+          children: (
+            <div>
+              {child.props.children}
+              {index < children.length - 1 && (
+                <StyledSvg>
+                  <g stroke={tokens.color_blue_700} stroke-width="2">
+                    <path
+                      stroke-dasharray="3,7"
+                      d="M1 2 l0 34"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                    />
+                  </g>
+                </StyledSvg>
+              )}
+            </div>
+          ),
+        });
       })}
-      <svg>
-        <g stroke={tokens.color_blue_700} stroke-width="2" fill="green" fill-rule="evenodd">
-          <path stroke-dasharray="4,6" d="M1 2 l0 100%" stroke-width="2" stroke-linecap="round" />
-        </g>
-      </svg>
     </StyledList>
   );
 });
