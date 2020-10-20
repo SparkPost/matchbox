e19fddd3bf79e85cff3e19d000698ba8746d1c10
Jon Ambas <jon@jonambas.com>
2020-10-20 12:13:22 -0400

    Add backgrounds

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -13,4 +13,25 @@ export const decorators = [withThemeProvider];
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
+  backgrounds: {
+    default: 'white',
+    values: [
+      {
+        name: 'white',
+        value: '#ffffff',
+      },
+      {
+        name: 'black',
+        value: '#000000',
+      },
+      {
+        name: 'gray.1000',
+        value: '#2c353d',
+      },
+      {
+        name: 'gray.100',
+        value: '#f5f8fa',
+      },
+    ],
+  },
 };
