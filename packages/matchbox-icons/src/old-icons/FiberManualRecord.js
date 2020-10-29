00d66aa971fb2839c34f2d64b0d22f917c5277fa
Jon Ambas <jon@jonambas.com>
2020-10-29 13:13:52 -0400

    UX-361 Add icons that are not present in new library

--- packages/matchbox-icons/src/old-icons/FiberManualRecord.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { createSvgIcon } from '../IconBase';
-
-export default createSvgIcon(
-  <g>
-    <circle cx="12" cy="12" r="8" />
-  </g>,
-  'FiberManualRecord',
-);
--- packages/matchbox-icons/src/old-icons/VolumeMute.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { createSvgIcon } from '../IconBase';
-
-export default createSvgIcon(
-  <g>
-    <path d="M7 9v6h4l5 5V4l-5 5H7z" />
-  </g>,
-  'VolumeMute',
-);
