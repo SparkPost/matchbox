13a3c8109c00dbb78873c53afb3f99475b70ff2b
Jon Ambas <jon@jonambas.com>
2020-11-05 10:49:57 -0500

    UX-362 Test attr instead of css

--- cypress/integration/Layout.spec.js
+++ cypress/integration/Layout.spec.js
@@ -4,14 +4,14 @@ describe('Layout', () => {
   it('should render non-mobile layout', () => {
     cy.viewport(1500, 500);
     cy.visit('/iframe.html?path=/story/layout-layout--annotated-example');
-    cy.get('[data-id="annotated-section"]').should('have.css', 'width', '345px');
-    cy.get('[data-id="detail-section"]').should('have.css', 'width', '1035px');
+    cy.get('[data-id="annotated-section"]').should('have.attr', 'width', '0.3333333333333333');
+    cy.get('[data-id="detail-section"]').should('have.attr', 'width', '1');
   });
 
   it('should render mobile layout', () => {
     cy.viewport(500, 500);
     cy.visit('/iframe.html?path=/story/layout-layout--annotated-example');
-    cy.get('[data-id="annotated-section"]').should('have.css', 'width', '404px');
-    cy.get('[data-id="detail-section"]').should('have.css', 'width', '404px');
+    cy.get('[data-id="annotated-section"]').should('have.attr', 'width', '100%');
+    cy.get('[data-id="detail-section"]').should('have.attr', 'width', '100%');
   });
 });
