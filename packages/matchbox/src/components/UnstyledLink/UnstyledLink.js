7b0a9e25876417359576c44087e30e4e105de830
Logan Sparlin <logan.sparlin@gmail.com>
2020-12-01 10:22:16 -0500

    UX-383 add disabled prop and styles to UnstyledLink (#702)
    Co-authored-by: Logan Sparlin <losparlin@Logan-Sparlins-MacBook-Pro.local>

--- packages/matchbox/src/components/UnstyledLink/UnstyledLink.js
+++ packages/matchbox/src/components/UnstyledLink/UnstyledLink.js
@@ -1,11 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import { deprecate } from '../../helpers/propTypes';
 
 import { Text } from '../Text';
 
+const StyledText = styled(Text)`
+  ${props => {
+    if (props.disabled) {
+      return `
+        opacity: 0.6;
+        color: ${props.theme.colors.gray['900']};
+        &:hover {
+          color: ${props.theme.colors.gray['900']};
+          cursor: not-allowed;
+        }
+    `;
+    }
+  }}
+`;
+
 const UnstyledLink = React.forwardRef(function UnstyledLink(props, ref) {
-  const { children, to, title, Component, component, external, onClick, role, ...rest } = props;
+  const {
+    children,
+    to,
+    title,
+    Component,
+    component,
+    external,
+    onClick,
+    role,
+    disabled,
+    ...rest
+  } = props;
+  console.log(disabled);
 
   const WrapperComponent = component || Component;
   const linkTitle = external && !title ? 'Opens in a new tab' : title;
@@ -13,7 +41,7 @@ const UnstyledLink = React.forwardRef(function UnstyledLink(props, ref) {
 
   if (to && !WrapperComponent) {
     return (
-      <Text
+      <StyledText
         as="a"
         href={to}
         target={external ? '_blank' : ''}
@@ -21,34 +49,44 @@ const UnstyledLink = React.forwardRef(function UnstyledLink(props, ref) {
         title={linkTitle}
         onClick={onClick}
         role={role}
+        disabled={disabled}
         ref={ref}
         {...rest}
       >
         {children}
-      </Text>
+      </StyledText>
     );
   }
 
   if (WrapperComponent) {
     return (
-      <Text
+      <StyledText
         as={WrapperComponent}
         onClick={onClick}
         ref={ref}
         role={role}
+        disabled={disabled}
         title={linkTitle}
         to={to}
         {...rest}
       >
         {children}
-      </Text>
+      </StyledText>
     );
   }
 
   return (
-    <Text as="a" title={linkTitle} role={linkRole} onClick={onClick} ref={ref} {...rest}>
+    <StyledText
+      as="a"
+      title={linkTitle}
+      role={linkRole}
+      onClick={onClick}
+      ref={ref}
+      disabled={disabled}
+      {...rest}
+    >
       {children}
-    </Text>
+    </StyledText>
   );
 });
 
@@ -56,6 +94,7 @@ UnstyledLink.displayName = 'UnstyledLink';
 
 UnstyledLink.propTypes = {
   to: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+  disabled: PropTypes.bool,
   title: PropTypes.string,
   external: PropTypes.bool,
   component: PropTypes.elementType,
