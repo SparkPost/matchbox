a534c13f03ef6c53aa60b0976ba6620971745dbb
Jon Ambas <jon@jonambas.com>
2020-12-10 14:39:03 -0500

    UX-403 & Issue-710 Fix empty Modal.Footer & dynamic children

--- packages/matchbox/src/components/Modal/Footer.js
+++ packages/matchbox/src/components/Modal/Footer.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { getChild } from '../../helpers/children';
 import { Panel } from '../Panel';
 import { Box } from '../Box';
+import { Inline } from '../Inline';
 
 const buttonProps = {
   0: {
     color: 'blue',
-    mr: '400',
+    key: 'modal-primary',
   },
   1: {
     color: 'blue',
-    outlineBorder: true,
-    mr: '400',
+    variant: 'outline',
+    key: 'modal-secondary',
   },
   2: {
     color: 'blue',
-    flat: true,
+    variant: 'text',
+    key: 'modal-tertiary',
   },
 };
 
@@ -28,20 +29,24 @@ function getButtonProps(index, element) {
 }
 
 const Footer = React.forwardRef(function Footer({ children }, ref) {
-  let buttons = getChild('Button', children);
+  const buttons = React.Children.toArray(children);
 
   return (
     <Panel borderLeft="none" borderRight="none" borderBottom="none" ref={ref}>
       <Panel.Section>
-        <Box display="flex" justifyContent="space-between">
-          <Box>
-            {buttons[0] && React.cloneElement(buttons[0], getButtonProps(0, buttons[0]))}
-            {buttons[1] && React.cloneElement(buttons[1], getButtonProps(1, buttons[1]))}
+        {buttons && (
+          <Box display="flex" justifyContent="space-between">
+            <Inline space="300">
+              {buttons[0] && React.cloneElement(buttons[1], getButtonProps(0, buttons[0]))}
+              {buttons[1] && React.cloneElement(buttons[1], getButtonProps(1, buttons[1]))}
+            </Inline>
+            {buttons[2] && (
+              <Box>
+                {buttons[2] && React.cloneElement(buttons[2], getButtonProps(2, buttons[2]))}
+              </Box>
+            )}
           </Box>
-          {buttons[2] && (
-            <Box>{buttons[2] && React.cloneElement(buttons[2], getButtonProps(2, buttons[2]))}</Box>
-          )}
-        </Box>
+        )}
       </Panel.Section>
     </Panel>
   );
