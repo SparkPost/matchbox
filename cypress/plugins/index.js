356a6827544236de33ccb074e77d55a685d095d9
Jon Ambas <jon@jonambas.com>
2021-01-26 16:47:58 -0500

    UX-425 Remove codecov integration

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -12,9 +12,6 @@
 // the project's config changing)
 
 module.exports = (on, config) => {
-  require('@cypress/code-coverage/task')(on, config);
-  on('file:preprocessor', require('@cypress/code-coverage/use-babelrc'));
-
   // Allows logging output while in headless mode
   // cy.task('log', 'This will be logged')
   on('task', {
