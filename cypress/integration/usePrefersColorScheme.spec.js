a7f2f4fdf75090bb8be98dce9897e09b1b3d0adb
Logan Sparlin <logan.sparlin@gmail.com>
2021-02-01 11:19:55 -0600

    UX-260 update tests to match both dark and light

--- cypress/integration/usePrefersColorScheme.spec.js
+++ cypress/integration/usePrefersColorScheme.spec.js
@@ -1,30 +1,31 @@
 /// <reference types="Cypress" />
 
 describe('usePrefersColorScheme hook', () => {
-  beforeEach(() => {
+  const visit = dark => {
     cy.visit('/iframe.html?path=/story/utility-usepreferscolorscheme--example-usage', {
-      onBeforeLoad(win) {
-        cy.stub(win, 'matchMedia')
+      onBeforeLoad(window) {
+        cy.stub(window, 'matchMedia')
           .withArgs('(prefers-color-scheme: dark)')
           .returns({
-            matches: true,
+            matches: dark,
           });
       },
     });
+  };
+  beforeEach(() => {
     // Setting viewport dimensions to avoid side effects
     cy.viewport(500, 500);
   });
 
-  it('should set dark or light', () => {
-    const DARK = '(prefers-reduced-motion: dark)';
-    const LIGHT = '(prefers-reduced-motion: light)';
+  it('should return dark if dark preference set', () => {
+    visit(true);
 
-    if (window.matchMedia(DARK).matches) {
-      cy.contains('reduce').should('exist');
-    }
+    cy.contains('dark').should('exist');
+  });
 
-    if (window.matchMedia(LIGHT).matches) {
-      cy.contains('no-preference').should('exist');
-    }
+  it('should return light if dark preference not set', () => {
+    visit(false);
+
+    cy.contains('light').should('exist');
   });
 });
