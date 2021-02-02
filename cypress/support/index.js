54327da06dc2afe92380c838c6cd1660bb160b0f
Jon Ambas <jon@jonambas.com>
2021-02-02 11:14:54 -0500

    UX-425 Remove codecov integration (#744)
    * UX-425 Remove codecov integration
    
    * UX-425 Remove coveralls and istanbul
    
    * UX-425 Remove cypress/code-coverage

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -16,9 +16,6 @@
 // Import commands.js using ES2015 syntax:
 import './commands';
 
-// Code coverage plugin
-import '@cypress/code-coverage/support';
-
 require('cypress-plugin-tab');
 
 // Alternatively you can use CommonJS syntax:
