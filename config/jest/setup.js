8e2a7e92b0623e02172133762afed6bf4558cf1e
Jon Ambas <jon@jonambas.com>
2021-02-25 12:25:31 -0500

    UX-380 Upgrade to React 17 (#697)
    * UX-380 Upgrade to React 17, Update package peer-deps
    
    * UX-380 Use third-party Enzyme adapter
    
    * UX-380 Update styled-components peer version

--- config/jest/setup.js
+++ config/jest/setup.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars, no-console */
 import './raf';
 import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import * as matchers from './matchers';
 import 'jest-styled-components';
 
