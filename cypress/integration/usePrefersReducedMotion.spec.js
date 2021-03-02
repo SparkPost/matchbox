5851587af4e5f1c74dd440c7fbcf83bfd6a2bd07
Jon Ambas <jon@jonambas.com>
2021-03-02 12:53:29 -0500

    UX-437 Fix usePrefersReducedMotion integration test

--- cypress/integration/usePrefersReducedMotion.spec.js
+++ cypress/integration/usePrefersReducedMotion.spec.js
@@ -1,6 +1,23 @@
 describe('usePrefersReducedMotion hook', () => {
-  it('should set no-preference', () => {
-    cy.visit('/iframe.html?path=usePrefersReducedMotion__example-usage&source=false');
+  const visit = reduce => {
+    cy.visit('iframe.html?path=usePrefersReducedMotion__example-usage&source=false', {
+      onBeforeLoad(window) {
+        cy.stub(window, 'matchMedia')
+          .withArgs('(prefers-reduced-motion: reduce)')
+          .returns({
+            matches: reduce,
+          });
+      },
+    });
+  };
+
+  it('should return reduce if reduce preference set', () => {
+    visit(true);
+    cy.contains('reduce').should('exist');
+  });
+
+  it('should return no-preference if no preference is set', () => {
+    visit(false);
     cy.contains('no-preference').should('exist');
   });
 });
