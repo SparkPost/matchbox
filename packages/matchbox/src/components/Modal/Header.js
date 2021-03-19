2c0f1b0970793b553abfba324b5971d40e996b3d
Jon Ambas <jon@jonambas.com>
2021-03-19 09:03:49 -0400

    UX-474 Replace Modal component cloning with React context

--- packages/matchbox/src/components/Modal/Header.js
+++ packages/matchbox/src/components/Modal/Header.js
@@ -7,13 +7,16 @@ import { ScreenReaderOnly } from '../ScreenReaderOnly';
 import { Box } from '../Box';
 import { Text } from '../Text';
 import { Button } from '../Button';
+import { ModalContext } from './context';
 
 const StyledButton = styled(Button)`
   color: ${tokens.color_gray_700};
 `;
 
 const Header = React.forwardRef(function Header(props, ref) {
-  const { children, onClose, showCloseButton } = props;
+  const { children, showCloseButton } = props;
+  const { onClose } = React.useContext(ModalContext);
+
   return (
     <Box data-id="modal-header" bg="white" pl="500" pr="500" pt="500" ref={ref}>
       <Box display="flex" alignItems="center">
