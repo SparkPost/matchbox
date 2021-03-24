5b3c6c6269036f0aff03ae09d390f0278e0dbe7c
Jon Ambas <jon@jonambas.com>
2021-03-24 16:39:56 -0400

    UX-474 Allow any Modal children (#794)
    * UX-474 Replace Modal component cloning with React context
    
    * UX-474 Test indirect children in libby

---
+++ packages/matchbox/src/components/Modal/context.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+/**
+ * Context is created here to pass modal onClose and open state to composed Modal components
+ */
+export const ModalContext = React.createContext({});
