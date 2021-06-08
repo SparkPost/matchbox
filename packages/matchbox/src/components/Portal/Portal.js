ec478fa30b4464da0ba0a204ffb9f3f94c254c60
Jon Ambas <jon@jonambas.com>
2021-06-08 19:30:16 -0400

    UX-565 Refactor Portal to see if this helps 2web2ui unit tests

--- packages/matchbox/src/components/Portal/Portal.js
+++ packages/matchbox/src/components/Portal/Portal.js
@@ -1,34 +1,29 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
 
-class Portal extends Component {
-  static displayName = 'Portal';
+function Portal(props) {
+  const { containerId, children } = props;
 
-  static propTypes = {
-    /**
-     * ID of the target portal container. Appends a new portal to document body if not provided.
-     */
-    containerId: PropTypes.string,
-    /**
-      * Content rendered inside the portal
-      */
-    children: PropTypes.node
-  };
-
-  render() {
-    const { containerId, children } = this.props;
-
-    if (typeof document !== 'undefined') {
-      const container = containerId
-        ? document.getElementById(containerId)
-        : document.body;
-
+  if (typeof document !== 'undefined') {
+    const container = containerId ? document.getElementById(containerId) : document.body;
+    if (container) {
       return ReactDOM.createPortal(children, container);
     }
-
-    return null;
   }
+
+  return null;
 }
+
+Portal.displayName = 'Portal';
+Portal.propTypes = {
+  /**
+   * ID of the target portal container. Appends a new portal to document body if not provided.
+   */
+  containerId: PropTypes.string,
+  /**
+   * Content rendered inside the portal
+   */
+  children: PropTypes.node,
+};
 
 export default Portal;
