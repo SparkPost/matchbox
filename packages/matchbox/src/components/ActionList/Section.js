36fa5b6d73de064f7624eca333dbcc7512238eb1
Jon Ambas <jon@jonambas.com>
2021-08-10 19:37:16 -0400

    UX-608 Revert ActionList.Section

--- packages/matchbox/src/components/ActionList/Section.js
+++ packages/matchbox/src/components/ActionList/Section.js
@@ -5,14 +5,7 @@ import { filterByVisible } from '../../helpers/array';
 
 import { StyledSection } from './styles';
 
-type Props = {
-  children?: React.ReactNode;
-  section?: any[];
-};
-
-type Ref = any;
-
-function SectionRender(props: Props, userRef: Ref) {
+const Section = React.forwardRef(function Section(props, userRef) {
   const { section, children } = props;
 
   if (children) {
@@ -20,7 +13,7 @@ function SectionRender(props: Props, userRef: Ref) {
   }
 
   const visibleActions = section
-    ? filterByVisible(section).map((action: any, i: number) => <Action key={i} {...action} />)
+    ? filterByVisible(section).map((action, i) => <Action key={i} {...action} />)
     : [];
 
   if (!visibleActions.length) {
@@ -28,12 +21,10 @@ function SectionRender(props: Props, userRef: Ref) {
   }
 
   return <StyledSection ref={userRef}>{visibleActions}</StyledSection>;
-}
-
-const Section = React.forwardRef<Ref, Props>(SectionRender);
+});
 
 Section.displayName = 'ActionList.Section';
-Section.propTypes = {
+Action.propTypes = {
   children: PropTypes.node,
 };
 
