16cee76d5026c9857b12188a61a65ca4e0663051
Jon Ambas <jon@jonambas.com>
2021-08-10 14:55:12 -0400

    UX-608 Setup matchbox for ts

--- packages/matchbox/rollup/config.js
+++ packages/matchbox/rollup/config.js
@@ -4,7 +4,7 @@ import jsPlugins from './plugins/js';
 import pkg from '../package.json';
 
 export const inputOptions = {
-  input: 'src/index.js',
+  input: 'src/index.ts',
   plugins: [...jsPlugins],
   external: [
     'copy-to-clipboard',
