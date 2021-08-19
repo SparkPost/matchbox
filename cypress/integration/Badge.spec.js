370d8d9a8979334ee679926910e702c4e4ffa395
Jon Ambas <jon@jonambas.com>
2021-08-19 12:58:18 -0400

    UX-611 Convert Badge to typescript (#913)
    * UX-611 Convert Badge to typescript
    
    * UX-611 Convert Badge story and Add margin system prop types
    
    * UX-611 Remove a unecessary test assertion
    
    * UX-611 Address feedback

--- cypress/integration/Badge.spec.js
+++ cypress/integration/Badge.spec.js
@@ -10,15 +10,12 @@ describe('The Badge component', () => {
     cy.visit('/iframe.html?path=Badge__e2e-test&source=false');
     cy.get('#test-id').should('have.css', 'margin-right', '32px');
     cy.get('#test-id-2').should('have.css', 'margin-top', '32px');
-    cy.get('#test-id-2').should('have.css', 'background-color', 'rgb(235, 240, 245)');
   });
 
   it('should set tabindex properly', () => {
     cy.visit('/iframe.html?path=Badge__with-a-tooltip&source=false');
     cy.get('body').tab();
     cy.focused().should('have.text', '100');
-    cy.findAllByText('I am a Tooltip')
-      .eq(1)
-      .should('be.visible');
+    cy.findAllByText('I am a Tooltip').eq(1).should('be.visible');
   });
 });
