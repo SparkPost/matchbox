64fe71d89f409424283ec4e117c6e3b3ee382be2
Jon Ambas <jon@jonambas.com>
2021-08-25 12:43:44 -0400

    UX-659 Add back in Text tests

--- packages/matchbox/src/components/Text/tests/Text.test.js
+++ packages/matchbox/src/components/Text/tests/Text.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Text from '../Text';
+import { tokens } from '@sparkpost/design-tokens';
 
 describe('Text', () => {
   it('it should render correctly', () => {
@@ -15,5 +16,42 @@ describe('Text', () => {
   it('should render data-id', () => {
     const wrapper = global.mountStyled(<Text data-id="test-id">Text</Text>);
     expect(wrapper.find('[data-id="test-id"]')).toExist();
+  });
+
+  describe('lookslike renders correctly', () => {
+    it('it should render visually as the lookslike prop while html should match the as prop ', () => {
+      const wrapper = global.mountStyled(
+        <div>
+          <Text as="h1" looksLike="h2">
+            Text
+          </Text>
+          <Text as="h2" looksLike="h5">
+            Text
+          </Text>
+          <Text as="h3" looksLike="h1">
+            Text
+          </Text>
+          <Text as="h4" looksLike="h6">
+            Text
+          </Text>
+          <Text as="h5" looksLike="h3">
+            Text
+          </Text>
+          <Text as="h6" looksLike="h4">
+            Text
+          </Text>
+          <Text as="p" looksLike="h1">
+            Text
+          </Text>
+        </div>,
+      );
+      expect(wrapper.find('h1')).toHaveStyleRule('font-size', tokens.fontSize_600);
+      expect(wrapper.find('h2')).toHaveStyleRule('font-size', tokens.fontSize_300);
+      expect(wrapper.find('h3')).toHaveStyleRule('font-size', tokens.fontSize_700);
+      expect(wrapper.find('h4')).toHaveStyleRule('font-size', tokens.fontSize_200);
+      expect(wrapper.find('h5')).toHaveStyleRule('font-size', tokens.fontSize_500);
+      expect(wrapper.find('h6')).toHaveStyleRule('font-size', tokens.fontSize_400);
+      expect(wrapper.find('p')).toHaveStyleRule('font-size', tokens.fontSize_700);
+    });
   });
 });
