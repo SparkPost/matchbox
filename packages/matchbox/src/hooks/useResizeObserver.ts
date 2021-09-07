7b1d1e5ba942e856af390311acfff611a6b3c54c
Jon Ambas <jon@jonambas.com>
2021-09-07 08:51:37 -0400

    UX-655 Little more specificity in useResizeObserver

--- packages/matchbox/src/hooks/useResizeObserver.ts
+++ packages/matchbox/src/hooks/useResizeObserver.ts
@@ -23,7 +23,22 @@ import ResizeObserver from 'resize-observer-polyfill';
  *  return <div ref={ref} />;
  * };
  */
-function useResizeObserver(): [React.Dispatch<any>, { contentRect?: { [k: string]: unknown } }] {
+function useResizeObserver(): [
+  React.Dispatch<any>,
+  {
+    target?: HTMLElement;
+    contentRect?: {
+      x?: number;
+      y?: number;
+      width?: number;
+      height?: number;
+      top?: number;
+      left?: number;
+      right?: number;
+      bottom?: number;
+    };
+  },
+] {
   const [entry, setEntry] = React.useState({ contentRect: {} });
   const [node, setNode] = React.useState(null);
   const observer = React.useRef(null);
