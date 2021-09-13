dc5964e326b197cc65166ec64b58e9687f75600a
Jon Ambas <jon@jonambas.com>
2021-09-13 14:18:07 -0400

    UX-670 Convert useInView to typescript

--- packages/matchbox/src/hooks/useInView.ts
+++ packages/matchbox/src/hooks/useInView.ts
@@ -6,19 +6,21 @@ import { getWindow } from '../helpers/window';
 /**
  * Reusable hook that returns true when element is scrolled into view
  *
- * @param {Boolean} once // defaults to true. If set to false the inView param will change when the element is scrolled into and out of view
+ * @param {Boolean} once defaults to true. If set to false the inView param will change when the element is scrolled into and out of view
  *
  * @example
  * const [ref, inView] = useInView({ once: false });
  * return <div>{inView ? 'In View' : 'Not In View'}</div>
  *
  */
-
-function useInView({ offset = 0, once = true } = {}) {
+function useInView<T extends HTMLElement>({
+  offset = 0,
+  once = true,
+}: { offset?: number; once?: boolean } = {}): [React.RefCallback<T>, boolean] {
   const [node, setNode] = React.useState(null);
   const [inView, setInView] = React.useState(false);
   const [scroll, setScroll] = React.useState(0);
-  const [rect, setRect] = React.useState({});
+  const [rect, setRect] = React.useState({ top: 0, height: 0 });
 
   const environment = getWindow();
 
