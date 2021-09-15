0d2463a7f1a9206e36769e3803e891dd81c8b615
Logan Sparlin <logan.sparlin@gmail.com>
2021-09-15 10:26:40 -0500

    UX-675 convert useTabs to typescript (#976)
    * UX-675 convert useTabs to typescript
    
    * UX-675 fix useTabs types

--- packages/matchbox/src/components/Tabs/index.ts
+++ packages/matchbox/src/components/Tabs/index.ts
@@ -1,1 +1,3 @@
 export { default as Tabs } from './Tabs';
+export * from './Tabs';
+export * from './Tab';
---
+++ packages/matchbox/src/hooks/useTabs.ts
@@ -0,0 +1,52 @@
+import React from 'react';
+import { TabProps } from '../components/Tabs';
+
+/**
+ * Reusable hook to be used with the Tabs component
+ */
+
+type UseTabsProps = {
+  tabs?: TabProps[];
+  initialSelected?: number;
+};
+
+function useTabs({ tabs = [], initialSelected = 0 }: UseTabsProps): {
+  getTabsProps: (userProps?: { [k: string]: unknown }) => {
+    tabs: TabProps[];
+    selected: number;
+    onSelect: React.EffectCallback;
+    [k: string]: unknown;
+  };
+  tabIndex: number;
+  tabs: TabProps[];
+  setTabIndex: React.Dispatch<React.SetStateAction<number>>;
+} {
+  const [tabIndex, setTabIndex] = React.useState<number>(initialSelected);
+
+  const tabsWithOnClick = React.useMemo(() => {
+    return tabs.map((tab, i) => ({ ...tab, onClick: () => setTabIndex(i) }));
+  }, [tabs]);
+
+  const onSelect = React.useCallback(
+    (index) => {
+      setTabIndex(index);
+    },
+    [tabs],
+  );
+
+  const getTabsProps = (userProps) => ({
+    tabs: tabsWithOnClick,
+    selected: tabIndex,
+    onSelect,
+    ...userProps,
+  });
+
+  return {
+    getTabsProps,
+    tabIndex,
+    tabs,
+    setTabIndex,
+  };
+}
+
+export default useTabs;
