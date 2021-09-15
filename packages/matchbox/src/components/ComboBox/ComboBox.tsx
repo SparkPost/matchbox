9557d3b07be4f3642208870f2f93154e9973b2fd
Logan Sparlin <logan.sparlin@gmail.com>
2021-09-15 08:39:29 -0500

    UX-619 convert ComboBox to typescript (#965)
    * UX-619 convert ComboBox to typescript
    
    * UX-619 replace getChild with helper and remove propTypes imports
    
    * UX-619 use getChild helper correctly

---
+++ packages/matchbox/src/components/ComboBox/ComboBox.tsx
@@ -0,0 +1,32 @@
+import React, { ReactNode } from 'react';
+import { getChild } from '../../helpers/children';
+import { Box } from '../Box';
+
+export type ComboBoxProps = {
+  children?: ReactNode;
+  style?: React.CSSProperties;
+  /**
+   * Maps to Downshift's refKey set in getRootProps. refKey must be set to "rootRef"
+   */
+  rootRef?: React.MutableRefObject<HTMLDivElement>;
+};
+
+function ComboBox(props: ComboBoxProps): JSX.Element {
+  const { children, style, rootRef, ...rest } = props;
+
+  // Clones children in order to pass the menu into the textfield
+  // Textfield inserts the menu in the DOM directly after the input, before helptext and error
+  const textfieldWithMenu = React.useMemo(() => {
+    return getChild('ComboBoxTextField', children, {
+      children: getChild('ComboBoxMenu', children),
+    });
+  }, [children]);
+
+  return (
+    <Box position="relative" ref={rootRef} style={style} {...rest}>
+      {textfieldWithMenu}
+    </Box>
+  );
+}
+
+export default ComboBox;
--- packages/matchbox/src/components/ComboBox/index.ts
+++ packages/matchbox/src/components/ComboBox/index.ts
@@ -1,3 +1,6 @@
 export { default as ComboBox } from './ComboBox';
 export { default as ComboBoxMenu } from './ComboBoxMenu';
 export { default as ComboBoxTextField } from './ComboBoxTextField';
+export * from './ComboBox';
+export * from './ComboBoxMenu';
+export * from './ComboBoxTextField';
