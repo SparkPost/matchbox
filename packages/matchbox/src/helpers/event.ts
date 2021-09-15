09448187f8fec82a2d690f6c1f8fdec42f00a014
Logan Sparlin <logan.sparlin@gmail.com>
2021-09-15 11:25:53 -0500

    UX-679 convert event helpers to typescript (#978)
    * UX-679 convert event helpers to typescript
    
    * UX-679 return undefined instead of null
    
    * UX-679 update to use React.TouchEvent
    
    * UX-679 revert back to regular TouchEvent
    
    * UX-679 fix text

---
+++ packages/matchbox/src/helpers/event.ts
@@ -0,0 +1,37 @@
+import React from 'react';
+
+export function debounce<T extends (...args: unknown[]) => unknown>(
+  func: T,
+  wait: number,
+  immediate?: boolean,
+) {
+  let timeout;
+  return function(...rest) { // eslint-disable-line
+    const context = this; // eslint-disable-line
+    const args = rest;
+    const later = function () {
+      timeout = null;
+      if (!immediate) {
+        func.apply(context, args);
+      }
+    };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) {
+      func.apply(context, args);
+    }
+  };
+}
+
+export function noop(): void {
+  return undefined;
+}
+
+export function identity<T>(a: T) {
+  return a;
+}
+
+export function isNotTouchEvent(e: TouchEvent): boolean {
+  return e.touches.length > 1 || (e.type.toLowerCase() === 'touchend' && e.touches.length > 0);
+}
