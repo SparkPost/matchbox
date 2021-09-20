78d81331fbe582f26010f595a75fe36f9332611c
Jon Ambas <jon@jonambas.com>
2021-09-20 10:29:00 -0400

    UX-696 Only render either content or children (#990)

--- cypress/integration/ActionList.spec.js
+++ cypress/integration/ActionList.spec.js
@@ -11,18 +11,26 @@ describe('ActionList component', () => {
   it('should render Actions correctly as buttons and links', () => {
     cy.get('button').should('have.length', 2);
     cy.get('a').should('have.length', 2);
+    cy.get('[role="menuitem"]').should('have.length', 4);
   });
 
   it('should handle disabled Actions correctly', () => {
-    cy.get('button')
-      .eq(1)
-      .should('be.disabled');
+    cy.get('button').eq(1).should('be.disabled');
+    cy.get('a').eq(1).should('have.attr', 'tabindex', '-1');
+    cy.get('a').eq(1).should('have.css', 'pointer-events', 'none');
+  });
+});
 
-    cy.get('a')
-      .eq(1)
-      .should('have.attr', 'tabindex', '-1');
-    cy.get('a')
-      .eq(1)
-      .should('have.css', 'pointer-events', 'none');
+describe('Deprecated ActionList', () => {
+  beforeEach(() => {
+    cy.visit(
+      '/iframe.html?path=ActionList-Deprecated__should-only-render-either-content-or-children',
+    );
+  });
+
+  it('should handle disabled Actions correctly', () => {
+    cy.get('a[role="menuitem"]').should('have.length', 2);
+    cy.get('a').eq(0).should('have.text', 'render me');
+    cy.get('a').eq(1).should('have.text', 'render me');
   });
 });
