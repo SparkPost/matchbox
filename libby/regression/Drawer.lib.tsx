df1807a27d308973e9cab5c5155d662e6d5f8c80
Jon Ambas <jon@jonambas.com>
2021-09-27 14:58:45 -0400

    UX-708 Take Percy snapshots of Drawer component (#1004)

---
+++ libby/regression/Drawer.lib.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { describe, add } from '@sparkpost/libby-react';
+import { Box, Drawer, Button, useDrawer, Tabs } from '@sparkpost/matchbox';
+
+describe('Drawer - Visual Regression', () => {
+  add('renders properly', () => {
+    const drawer = useDrawer();
+
+    return (
+      <>
+        <Button {...drawer.getActivatorProps()}>Open Drawer</Button>
+        <Drawer {...drawer.getDrawerProps()}>
+          <Drawer.Header>Drawer Title</Drawer.Header>
+          <Drawer.Content p="0">
+            <Tabs
+              fitted
+              selected={0}
+              tabs={[{ content: 'Metrics' }, { content: 'Filters' }, { content: 'Compare' }]}
+            />
+            <Box p="500">Content</Box>
+          </Drawer.Content>
+          <Drawer.Footer>
+            <Box display="flex">
+              <Box flex="1" pr="100">
+                <Button width="100%" color="blue">
+                  Apply
+                </Button>
+              </Box>
+              <Box flex="1" pl="100">
+                <Button width="100%" variant="outline">
+                  Cancel
+                </Button>
+              </Box>
+            </Box>
+          </Drawer.Footer>
+        </Drawer>
+      </>
+    );
+  });
+});
