d942b21d1578532917b5cb4df9278e751b8b4584
Jon Ambas <jon@jonambas.com>
2021-09-29 12:49:29 -0400

    issue-920 Back out WindowEvent change

--- packages/matchbox/src/components/WindowEvent/WindowEvent.tsx
+++ packages/matchbox/src/components/WindowEvent/WindowEvent.tsx
@@ -9,7 +9,7 @@ export type WindowEventProps<T extends keyof WindowEventMap> = {
   /**
    * Event callback function
    */
-  handler: React.EventHandler<any> | ((e: WindowEventMap[T]) => void);
+  handler: (e: WindowEventMap[T]) => void;
 };
 
 /**
