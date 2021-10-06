d6c5832e4dc3c7c53e91248a883a2807f14e241a
Logan Sparlin <logan.sparlin@gmail.com>
2021-10-06 14:43:30 -0500

    UX-707 add mobile snapshots for modal and legacy modal

--- cypress/regression/Modal.spec.js
+++ cypress/regression/Modal.spec.js
@@ -1,13 +1,29 @@
 describe('Modal component', () => {
-  it('renders correctly', () => {
+  it('renders correctly at full width', () => {
+    cy.viewport(1280, 375);
     cy.visit('/iframe.html?path=Visual-Regression__Modal');
     cy.get('button').first().click();
     cy.wait(500); // Wait for animation
-    cy.percySnapshot('Modal');
+    cy.percySnapshot('Modal - Desktop', { widths: [1280] });
   });
 
-  it('renders legacy modal correctly', () => {
+  it('renders correctly at mobile width', () => {
+    cy.viewport(375, 375);
+    cy.visit('/iframe.html?path=Visual-Regression__Modal');
+    cy.get('button').first().click();
+    cy.wait(500); // Wait for animation
+    cy.percySnapshot('Modal - Mobile', { widths: [375] });
+  });
+
+  it('renders legacy modal correctly at full width', () => {
+    cy.viewport(1280, 375);
     cy.visit('/iframe.html?path=Visual-Regression__Legacy-Modal');
-    cy.percySnapshot('Legacy Modal');
+    cy.percySnapshot('Legacy Modal - Desktop', { widths: [1280] });
+  });
+
+  it('renders legacy modal correctly at mobile width', () => {
+    cy.viewport(375, 375);
+    cy.visit('/iframe.html?path=Visual-Regression__Legacy-Modal');
+    cy.percySnapshot('Legacy Modal - Mobile', { widths: [375] });
   });
 });
