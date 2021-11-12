444f8dabcfca700520a3a5ab60c81180a5abeb2c
Jon Ambas <jon@jonambas.com>
2021-11-12 16:00:30 -0500

    UX-730 Fix tests that relied on passed system props

--- cypress/integration/Layout.spec.js
+++ cypress/integration/Layout.spec.js
@@ -2,14 +2,14 @@ describe('Layout', () => {
   it('should render non-mobile layout', () => {
     cy.viewport(1500, 500);
     cy.visit('/iframe.html?path=Layout__annotated-example-in-page&source=false');
-    cy.get('[data-id="annotated-section"]').should('have.attr', 'width', '0.3333333333333333');
-    cy.get('[data-id="detail-section"]').should('have.attr', 'width', '1');
+    cy.get('[data-id="annotated-section"]').should('have.css', 'width', '357px');
+    cy.get('[data-id="detail-section"]').should('have.css', 'width', '1071px');
   });
 
   it('should render mobile layout', () => {
     cy.viewport(500, 500);
     cy.visit('/iframe.html?path=Layout__annotated-example-in-page&source=false');
-    cy.get('[data-id="annotated-section"]').should('have.attr', 'width', '100%');
-    cy.get('[data-id="detail-section"]').should('have.attr', 'width', '100%');
+    cy.get('[data-id="annotated-section"]').should('have.css', 'width', '452px');
+    cy.get('[data-id="detail-section"]').should('have.css', 'width', '452px');
   });
 });
