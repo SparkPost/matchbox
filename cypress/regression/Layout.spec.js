03f48abeab21950a4685439309808595bec47f59
Jon Ambas <jon@jonambas.com>
2021-11-15 08:48:52 -0500

    UX-730 Consolidate regression snapshots

--- cypress/regression/Layout.spec.js
+++ cypress/regression/Layout.spec.js
@@ -1,12 +1,6 @@
 describe('Page, Layout, Columns', () => {
   it('should render non-mobile layout', () => {
     cy.visit('/iframe.html?path=Visual-Regression__Page-Layout-Columns');
-    cy.percySnapshot('Page, Layout, Columns', { widths: [1280] });
-  });
-
-  it('should render mobile layout', () => {
-    cy.viewport(500, 500);
-    cy.visit('/iframe.html?path=Visual-Regression__Page-Layout-Columns');
-    cy.percySnapshot('Page, Layout, Columns', { widths: [375] });
+    cy.percySnapshot('Page, Layout, Columns', { widths: [1280, 375] });
   });
 });
