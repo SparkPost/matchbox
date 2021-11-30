be0bfa8622f23bc1f9cee7fb93723793c04e206e
Jon Ambas <jon@jonambas.com>
2021-11-30 16:07:23 -0500

    UX-752 Add index.js file entry point

---
+++ packages/design-tokens-sd/index.js
@@ -0,0 +1,10 @@
+const tokens = require('./dist/js/tokens.legacy');
+const tokens_next = require('./dist/js/tokens');
+const meta = require('./dist/meta/meta');
+
+module.exports = {
+  tokens,
+  tokens_legacy: tokens,
+  tokens_next,
+  meta,
+};
