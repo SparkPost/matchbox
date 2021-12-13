14e57f23c7e16f6081b2e0bc744cc1d04f425e9e
Jon Ambas <jon@jonambas.com>
2021-12-13 12:02:15 -0500

    UX-754 Style Dictionary (#1029) (#1038)
    * UX-747 & UX-748: Add Style Dictionary and CSS custom properties file (#1026)
    
    * UX-747 Add Style Dictionary basic setup
    
    * UX-747 Add box-shadow
    
    * UX-747 Update to a temporary package name
    
    * UX-747 Update readme.md
    
    * UX-747 Fix spelling error
    
    * UX-750 Add SCSS map and function generation (#1027)
    
    * UX-750 Add SCSS map and function generation
    
    * UX-750 Interpolate string so quotes are not needed by end user
    
    * UX-750 Fix font size tokens
    
    * UX-749 Generate JS token files (#1028)
    
    * UX-749 Generate JS token files
    
    * UX-749 Update deprecated token file name
    
    * UX-749 Use 'legacy' instead of 'deprecated'
    
    * UX-752 Generate meta token file (#1030)
    
    * UX-752 Add meta file generation
    
    * UX-752 Add index.js file entry point
    
    * UX-752 Some clean up
    
    * UX-752 More clean up
    
    * UX-755 Harden design tokens (#1032)
    
    * UX-755 Harden design tokens
    
    * UX-755 Add sd package to lerna
    
    * UX-754 Prepare design token package
    
    * UX-754 Prepare design token package part 2
    
    * UX-754 Update readme
    
    * UX-754 Update snapshots
    
    * UX-754 Fix color-white and color casing in tests
    
    * UX-754 Fix color-white types

---
+++ packages/design-tokens/templates/scss-functions.js
@@ -0,0 +1,61 @@
+const MAP = 'matchbox-tokens';
+
+/**
+ * Generates SCSS map functions for all tokens aside from color
+ */
+const mapGet = (key) => `
+  @function ${key}($variant) {
+    $parent-map: map-get($${MAP}, '${key}');
+      
+    @if map-has-key($parent-map, '#{$variant}') {
+      @return map-get($parent-map, '#{$variant}');
+    }
+    
+    @error '${key} '#{$variant}' not found.';
+  }
+`;
+
+const utils = (rootFontSize) => {
+  const root = '$font-size-root';
+  const unitless = rootFontSize.value.replace('px', '');
+
+  return `
+    @import './maps';
+
+    $DEFAULT_FONT_SIZE: ${root};
+    $DEFAULT_FONT_SIZE_UNITLESS: ${unitless};
+
+    @function rem($n) {
+      $result: 0rem + $n / $DEFAULT_FONT_SIZE_UNITLESS;
+      @return $result;
+    }
+
+    @function em($n) {
+      $result: 0em + $n / $DEFAULT_FONT_SIZE_UNITLESS;
+      @return $result;
+    }
+  `;
+};
+
+const colorMapGet = () => `
+  @function color($palette, $variant) {
+    $color-map: map-get($${MAP}, 'color');
+
+    @if $palette == 'white' {
+      @return map-get($color-map, 'white');
+    }
+
+    @if map-has-key($color-map, '#{$palette}') {
+      $palette-map: map-get($color-map, '#{$palette}');
+      @return map-get($palette-map, '#{$variant}');
+    }
+    
+    @error 'color' #{$palette} - #{$variant}' not found.';
+  }
+`;
+
+module.exports = {
+  mapGet,
+  utils,
+  colorMapGet,
+};
--- packages/design-tokens/tests/tokens.test.js
+++ packages/design-tokens/tests/tokens.test.js
@@ -1,22 +1,20 @@
-import { tokens, meta } from '../';
-import path from 'path';
-import fs from 'fs';
+import { tokens_next, meta } from '../';
 
 it('should format a standard tokens correctly', () => {
   const token = meta.find(({ name }) => name === 'border-radius-200');
-  expect(tokens[token.javascript]).toEqual(token.value);
+  expect(tokens_next[token.javascript]).toEqual(token.value);
   expect(token).toMatchSnapshot();
 });
 
 it('should format a nested map tokens correctly', () => {
   const token = meta.find(({ name }) => name === 'color-green-700');
-  expect(tokens[token.javascript]).toEqual(token.value);
-  expect(token.scss).toEqual('color(green, 700)');
+  expect(tokens_next[token.javascript]).toEqual(token.value);
+  expect(token.scss).toEqual('color(green,700)');
 });
 
 it('should format a font size token correctly', () => {
   const token = meta.find(({ name }) => name === 'font-size-500');
-  expect(tokens[token.javascript]).toEqual(token.value);
+  expect(tokens_next[token.javascript]).toEqual(token.value);
   expect(token.value).toEqual('1.5rem');
   expect(token.pixel_value).toEqual('24px');
   expect(token.pixel_value_unitless).toEqual('24');
@@ -25,44 +23,11 @@ it('should format a font size token correctly', () => {
 
 it('should format root font size correctly', () => {
   const token = meta.find(({ name }) => name === 'font-size-root');
-  expect(tokens[token.javascript]).toEqual(token.value);
+  expect(tokens_next[token.javascript]).toEqual(token.value);
   expect(token.value).toEqual('16px');
 });
 
 it('should format consistently', () => {
-  expect(tokens).toMatchSnapshot();
+  expect(tokens_next).toMatchSnapshot();
   expect(meta).toMatchSnapshot();
-});
-
-// Non-JS File Snapshots
-
-function readFile(file) {
-  return fs.readFileSync(path.join(process.cwd(), `packages/design-tokens/dist/${file}`), 'utf-8');
-}
-
-it('should generate CSS', () => {
-  const file = readFile('index.custom-properties.css');
-  expect(file).toMatchSnapshot();
-});
-
-it('should generate SCSS', () => {
-  const files = [
-    'border-radius.map.scss',
-    'border-width.map.scss',
-    'box-shadow.map.scss',
-    'font-family.map.scss',
-    'font-size.map.scss',
-    'font-weight.map.scss',
-    'line-height.map.scss',
-    'media-query.map.scss',
-    'motion-duration.map.scss',
-    'motion-ease.map.scss',
-    'sizing.map.scss',
-    'spacing.map.scss',
-    'z-index.map.scss',
-    'color.deep-map.scss',
-  ];
-  for (const file of files) {
-    expect(readFile(file)).toMatchSnapshot();
-  }
 });
