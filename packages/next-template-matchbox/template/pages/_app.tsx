9c3a3ae898462c85e4fc096db6790ff39894037f
Logan Sparlin <logan.sparlin@gmail.com>
2022-01-05 13:54:20 -0600

    UX-757 Create Next App Templatels

--- packages/next-template-matchbox/template/pages/_app.tsx
+++ packages/next-template-matchbox/template/pages/_app.tsx
@@ -1,8 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from '@sparkpost/matchbox';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <ThemeProvider>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp
