76015b993696488f7d9632feda6bef0df534ed53
Logan Sparlin <logan.sparlin@gmail.com>
2022-01-06 10:32:58 -0600

    UX-757 Nextjs template (#1049)
    * UX-757 Create Next App Templatels
    
    * UX-757 Create Next App Templatels
    
    * UX-757 remove vercel.svg
    
    * UX-757 remove ! after Next.js
    
    * remove unused styled

---
+++ packages/next-template-matchbox/template/pages/_app.tsx
@@ -0,0 +1,12 @@
+import type { AppProps } from 'next/app'
+import { ThemeProvider } from '@sparkpost/matchbox';
+
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <ThemeProvider>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
+
+export default MyApp
