7fdcd8402bb05981e57e929f3a23a0d901b8c7c1
Jon Ambas <jon@jonambas.com>
2022-08-24 11:50:18 -0400

    build: fix type compilation

--- packages/matchbox-icons/rollup/plugins/js.js
+++ packages/matchbox-icons/rollup/plugins/js.js
@@ -2,7 +2,7 @@
 
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
-import { terser } from 'rollup-plugin-terser';
+// import { terser } from 'rollup-plugin-terser';
 import typescript from '@rollup/plugin-typescript';
 import { DEFAULT_EXTENSIONS } from '@babel/core';
 
@@ -16,6 +16,6 @@ export default [
     presets: ['@babel/env', '@babel/react', '@babel/preset-typescript'],
     plugins: ['@babel/proposal-object-rest-spread', '@babel/proposal-class-properties'],
   }),
-  terser(),
+  // terser(),
   typescript({ tsconfig: './tsconfig.json' }),
 ];
