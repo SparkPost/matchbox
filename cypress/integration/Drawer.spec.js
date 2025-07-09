8e7827e56d9c15214916b509fa319bcc8d6b723d
Doug Koerich <douglas.koerich@bird.com>
2025-07-09 14:24:54 -0300

    TASK-10510: Debugging Drawer test
    Signed-off-by: Doug Koerich <douglas.koerich@bird.com>

--- cypress/integration/Drawer.spec.js
+++ cypress/integration/Drawer.spec.js
@@ -28,22 +28,28 @@ describe('Drawer component', () => {
   });
 
   it('traps focus', () => {
-    cy.wait(5000); // waits for Drawer to enter
+    cy.wait(500); // waits for Drawer to enter
     cy.get('body').tab();
+    cy.focused().then($el => { console.log('FOCUSED ELEMENT:', $el[0]); });
     cy.focused().should('have.text', 'Close');
     cy.focused().tab();
+    cy.focused().then($el => { console.log('FOCUSED ELEMENT:', $el[0]); });
     cy.focused().should('have.text', 'Button 1');
     cy.focused().tab();
+    cy.focused().then($el => { console.log('FOCUSED ELEMENT:', $el[0]); });
     cy.focused().should('have.text', 'Button 2');
     cy.focused().tab();
+    cy.focused().then($el => { console.log('FOCUSED ELEMENT:', $el[0]); });
     cy.focused().should('have.text', 'Close');
   });
 
   it('returns focus on close', () => {
-    cy.wait(5000); // waits for Drawer to enter
+    cy.wait(500); // waits for Drawer to enter
     cy.get('body').tab();
+    cy.focused().then($el => { console.log('FOCUSED ELEMENT:', $el[0]); });
     cy.focused().should('have.text', 'Close');
     cy.get('body').type('{esc}');
+    cy.focused().then($el => { console.log('FOCUSED ELEMENT:', $el[0]); });
     cy.focused().should('have.text', 'On the right');
   });
 });
